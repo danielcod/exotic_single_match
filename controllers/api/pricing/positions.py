4425f1e7de8be08c97fcde36fdc031aab03345ec
Justin <justin.worrall@gmail.com>
2017-01-05 12:39:34 +0000

    added parse_list

--- controllers/api/pricing/positions.py
+++ controllers/api/pricing/positions.py
@@ -1,27 +1,67 @@
 from controllers.api.pricing import *
 
+Winner, Top, Bottom = "Winner", "Top", "Bottom"
+
+Today=datetime.date.today()
+
 # curl "http://localhost:8080/api/pricing/positions?league=ENG.1&team=Arsenal&teams=Arsenal,Liverpool,Man%20Utd&payoff=Winner&use_results=true&expiry=2017-03-01"
 
 class IndexHandler(webapp2.RequestHandler):
 
-    """
-    - this will need heavily unit testing
-    """
+    def parse_boolean(self, boolstr):
+        return boolstr.lower() in ["true", "1"]
     
-    def get_payoff_index(self, payoff):
-        if payoff=="Winner":
+    def parse_list(self, liststr):
+        return liststr.split(",")
+
+    def parse_expiry(self, events, expirystr):
+        if expirystr==EOS:
+            return sorted([event.date
+                           for event in events])[-1]
+        elif re.search("^\\d{4}\\-\\d{1,2}\\-\\d{1,2}$", expirystr):
+            return datetime.datetime.strptime(expirystr, "%Y-%m-%d").date()
+        else:
+            raise RuntimeError("Couldn't parse '%s' as expiry" % expirystr)
+
+    def parse_payoff(self, payoff):
+        if payoff==Winner:
             return [0]
-        elif re.search("Top \\d+", payoff):
+        elif re.search(Top+" \\d+", payoff):
             n=int(re.findall("\\d+", payoff)[0])
             return [i for i in range(n)]
-        elif re.search("Bottom \\d+", payoff):
+        elif re.search(Bottom+" \\d+", payoff):
             n=int(re.findall("\\d+", payoff)[0])
             return [-(1+i) for i in range(n)]
-        elif payoff=="Bottom":
+        elif payoff==Bottom:
             return [-1]
         else:
             raise RuntimeError("Payoff not recognised")
+            
+    def filter_teams(self, teams, teamnames):
+        if All in teamnames:
+            return teams
+        return [team for team in teams
+                if team["name"] in teamnames]
     
+    def filter_results(self, results, teamnames):
+        def filterfn(result):
+            matchteamnames=result["name"].split(" vs ")
+            return (matchteamnames[0] in teamnames or
+                    matchteamnames[1] in teamnames)
+        return [result for result in results
+                if filterfn(result)]
+
+    def filter_events(self, events, expirydate, startdate=Today):
+        return [event for event in events
+                if event.date > startdate and event.date <= expirydate]
+
+    def filter_remaining_fixtures(self, remfixtures, events):
+        eventnames=[event.name
+                    for event in events]
+        return [remfixture
+                for remfixture in remfixtures
+                if remfixture["name"] in eventnames]
+
     @validate_query({'league': '^\\D{3}\\.\\d$',
                      'team': '.+',
                      'teams': '.*',
@@ -30,67 +70,28 @@ class IndexHandler(webapp2.RequestHandler):
                      'expiry': '^(\\d{4}\\-\\d{1,2}\\-\\d{1,2})|(EOS)$'})
     @emit_json
     def get(self):
-        # unpack request
+        # unpack league
         leaguename=self.request.get("league")
-        teamname=self.request.get("team")
-        teamnames=self.request.get("teams")
-        payoff=self.request.get("payoff")
-        useresults=eval(self.request.get("use_results").capitalize()) # because bool("false")==True :-/
-        expiry=self.request.get("expiry")
-        # teams
+        # fetch data
         allteams=yclite.get_teams(leaguename)
-        allteamnames=[team["name"]
-                      for team in allteams]
-        if teamname not in allteamnames:
-            raise RuntimeError("%s not found" % teamname)
-        if teamnames==All:
-            teamnames=allteamnames
-        else:
-            teamnames=teamnames.split(",")
-            for _teamname in teamnames: # NB _teamname
-                if _teamname not in allteamnames:
-                    raise RuntimeError("%s not found" % _teamname)
-        teams=[team for team in allteams
-               if team["name"] in teamnames]
-        logging.info("%i teams" % len(teams))
-        # results
-        allresults=yclite.get_results(leaguename)        
-        if useresults:
-            def filterfn(result):
-                matchteamnames=result["name"].split(" vs ")
-                return (matchteamnames[0] in teamnames or
-                        matchteamnames[1] in teamnames)
-            results=[result for result in allresults
-                     if filterfn(result)]
-        else:
-            results=[]
-        logging.info("%i results" % len(results))
-        # events
-        today=datetime.date.today()
-        allevents=sorted(Event.find_all(leaguename),
-                         key=lambda x: x.date)
-        if expiry==EOS:
-            expiry=allevents[-1].date
-        else:
-            expiry=datetime.datetime.strptime(expiry, "%Y-%m-%d").date()
-        events=[event for event in allevents
-                if event.date > today and event.date <= expiry]
-        logging.info("%i events" % len(events))
-        # remfixtures
+        allresults=yclite.get_results(leaguename)
         allremfixtures=yclite.get_remaining_fixtures(leaguename)
-        eventnames=[event.name
-                    for event in events]
-        remfixtures=[remfixture
-                     for remfixture in allremfixtures
-                     if remfixture["name"] in eventnames]
-        logging.info("%i rem fixtures" % len(remfixtures))
+        allevents=Event.find_all(leaguename)                
+        # unpacket other params
+        teamname=self.request.get("team")
+        teamnames=self.parse_list(self.request.get("teams"))
+        payoff=self.parse_payoff(self.request.get("payoff"))
+        useresults=self.parse_boolean(self.request.get("use_results"))
+        expiry=self.parse_expiry(allevents, self.request.get("expiry"))
+        # filter data
+        teams=self.filter_teams(allteams, teamnames)
+        results=self.filter_results(allresults, teamnames) if useresults else []
+        events=self.filter_events(allevents, expiry)
+        remfixtures=self.filter_remaining_fixtures(allremfixtures, events)
         # pricing        
-        index=self.get_payoff_index(payoff)
-        logging.info("index %s" % index)
         pp=simulator.simulate(teams, results, remfixtures, Paths, Seed)
         prob=sum([pp[teamname][i]
-                  for i in index])
-        logging.info("probability %.5f" % prob)
+                  for i in payoff])
         if prob < MinProb or prob > MaxProb:
             price=None
         else:
