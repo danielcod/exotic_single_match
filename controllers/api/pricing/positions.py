aa6c8e0b68663923bb9a9073a0883191a2106042
Justin <justin.worrall@gmail.com>
2017-01-05 13:02:06 +0000

    now returning all data

--- controllers/api/pricing/positions.py
+++ controllers/api/pricing/positions.py
@@ -14,14 +14,14 @@ class IndexHandler(webapp2.RequestHandler):
     def parse_list(self, liststr):
         return liststr.split(",")
 
-    def parse_expiry(self, events, expirystr):
+    def parse_date(self, events, expirystr):
         if expirystr==EOS:
-            return sorted([event.date
+            return sorted([event["date"]
                            for event in events])[-1]
         elif re.search("^\\d{4}\\-\\d{1,2}\\-\\d{1,2}$", expirystr):
             return datetime.datetime.strptime(expirystr, "%Y-%m-%d").date()
         else:
-            raise RuntimeError("Couldn't parse '%s' as expiry" % expirystr)
+            raise RuntimeError("Couldn't parse '%s' as date" % expirystr)
 
     def parse_payoff(self, payoff):
         if payoff==Winner:
@@ -53,20 +53,26 @@ class IndexHandler(webapp2.RequestHandler):
 
     def filter_events(self, events, expirydate, startdate=Today):
         return [event for event in events
-                if event.date > startdate and event.date <= expirydate]
+                if (event["date"] > startdate and
+                    event["date"] <= expirydate)]
 
     def filter_remaining_fixtures(self, remfixtures, events):
-        eventnames=[event.name
+        eventnames=[event["name"]
                     for event in events]
         return [remfixture
                 for remfixture in remfixtures
                 if remfixture["name"] in eventnames]
 
+    def calc_probability(self, pp, index):
+        return sum([pp[i] for i in index])
+
+    def calc_price(self, prob, maxprob=MaxProb, minprob=MinProb):
+        return 1/float(prob) if (prob > minprob and prob < maxprob) else None
+    
     @validate_query({'league': '^\\D{3}\\.\\d$',
                      'team': '.+',
                      'teams': '.*',
                      'payoff': '^(Winner)|(Top \\d+)|(Bottom \\d+)|(Bottom)$',
-                     'use_results': '^(true)|(false)$',
                      'expiry': '^(\\d{4}\\-\\d{1,2}\\-\\d{1,2})|(EOS)$'})
     @emit_json
     def get(self):
@@ -76,28 +82,32 @@ class IndexHandler(webapp2.RequestHandler):
         allteams=yclite.get_teams(leaguename)
         allresults=yclite.get_results(leaguename)
         allremfixtures=yclite.get_remaining_fixtures(leaguename)
-        allevents=Event.find_all(leaguename)                
-        # unpacket other params
+        allevents=[event.to_json()
+                   for event in Event.find_all(leaguename)]                
+        # unpacket request
         teamname=self.request.get("team")
         teamnames=self.parse_list(self.request.get("teams"))
+        if teamname not in teamnames:
+            teamnames+=teamname
         payoff=self.parse_payoff(self.request.get("payoff"))
-        useresults=self.parse_boolean(self.request.get("use_results"))
-        expiry=self.parse_expiry(allevents, self.request.get("expiry"))
+        expiry=self.parse_date(allevents, self.request.get("expiry"))
         # filter data
         teams=self.filter_teams(allteams, teamnames)
-        results=self.filter_results(allresults, teamnames) if useresults else []
+        results=self.filter_results(allresults, teamnames) # could be []
         events=self.filter_events(allevents, expiry)
         remfixtures=self.filter_remaining_fixtures(allremfixtures, events)
+        if remfixtures==[]:
+            raise RuntimeError("No remaining fixtures")
         # pricing        
         pp=simulator.simulate(teams, results, remfixtures, Paths, Seed)
-        prob=sum([pp[teamname][i]
-                  for i in payoff])
-        if prob < MinProb or prob > MaxProb:
-            price=None
-        else:
-            price=1/float(prob)
-        # return
-        return {"price": price}
+        probability=self.calc_probability(pp[teamname], payoff)
+        price=self.calc_price(probability)
+        return {"teams": teams,
+                "results": results,
+                "events": events,
+                "remaining_fixtures": remfixtures,
+                "probability": probability,
+                "price": price}
 
 Routing=[('/api/pricing/positions', IndexHandler)]
 
