9a99c7978c6d3c42a6ad1e87a585d8e4e83699a2
Justin <justin.worrall@gmail.com>
2017-01-06 15:13:45 +0000

    converted mini leagues to use json post

--- controllers/api/pricing/positions/mini_leagues.py
+++ controllers/api/pricing/positions/mini_leagues.py
@@ -1,23 +1,18 @@
 from controllers.api.pricing.positions import *
 
-# curl "http://localhost:8080/api/pricing/positions/mini_leagues?league=ENG.1&team=Arsenal&teams=Arsenal,Liverpool,Man%20Utd&payoff=Winner&expiry=2017-03-01"
+# curl -X POST "http://localhost:8080/api/pricing/positions/mini_leagues" -d "{\"teams\": [{\"league\": \"ENG.1\", \"name\": \"Arsenal\"}, {\"league\": \"ENG.1\", \"name\": \"Liverpool\"}, {\"league\": \"ENG.1\", \"name\": \"Man Utd\"}], \"payoff\": \"Winner\", \"expiry\": \"2017-03-01\"}"
 
 class IndexHandler(webapp2.RequestHandler):
 
-    @validate_query({'league': '^\\D{3}\\.\\d$',
-                     'team': '.+',
-                     'teams': '.*',
-                     'payoff': '^(Winner)|(Top \\d+)|(Bottom \\d+)|(Bottom)$',
-                     'expiry': '^(\\d{4}\\-\\d{1,2}\\-\\d{1,2})|(EOS)$'})
+    @parse_json_body
     @emit_json
-    def get(self, results=[]): # NB no initial results state
-        leaguename=self.request.get("league")
-        teamname=self.request.get("team")
-        teamnames=parse_list(self.request.get("teams"))
-        if teamname not in teamnames:
-            teamnames+=teamname
-        payoff=self.request.get("payoff")
-        expirystr=self.request.get("expiry")
+    def post(self, struct, results=[]): # NB no initial results state
+        leaguename=struct["teams"][0]["league"] # TEMP
+        teamname=struct["teams"][0]["name"] # TEMP
+        teamnames=[team["name"]
+                   for team in struct["teams"]]
+        payoff=struct["payoff"]
+        expirystr=struct["expiry"]
         allteams=fetch_teams(leaguename)
         validate_teamnames(allteams, teamnames)
         teams=filter_teams(allteams, teamnames)
