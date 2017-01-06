de47d32e245492a13cd7e2fe7db962c31a54cba1
Justin <justin.worrall@gmail.com>
2017-01-06 15:53:03 +0000

    simplified teams filterfn

--- controllers/api/pricing/positions/mini_leagues.py
+++ controllers/api/pricing/positions/mini_leagues.py
@@ -3,7 +3,7 @@ from controllers.api.pricing.positions import *
 # curl -X POST "http://localhost:8080/api/pricing/positions/mini_leagues" -d "{\"teams\": [{\"league\": \"ENG.1\", \"name\": \"Arsenal\", \"selected\": true}, {\"league\": \"ENG.1\", \"name\": \"Liverpool\"}, {\"league\": \"ENG.1\", \"name\": \"Man Utd\"}], \"payoff\": \"Winner\", \"expiry\": \"2017-03-01\"}"
 
 class IndexHandler(webapp2.RequestHandler):
-
+    
     def filter_selected_team(self, teams):
         for team in teams:
             if ("selected" in team and
@@ -14,24 +14,28 @@ class IndexHandler(webapp2.RequestHandler):
     def filter_fixtures(self, fixtures, teams, expirydate, startdate=Today):
         teamnames=[team["name"]
                    for team in teams]
-        def filterfn(result):
-            matchteamnames=result["name"].split(" vs ")
-            return (matchteamnames[0] in teamnames or
-                    matchteamnames[1] in teamnames)
-        return [fixture for fixture in fixtures
-                if (filterfn(fixture) and
+        def filterfn(fixtures):
+            matchteamnames=fixtures["name"].split(" vs ")
+            return ((matchteamnames[0] in teamnames or
+                     matchteamnames[1] in teamnames) and
                     fixture["date"] > startdate and
-                    fixture["date"] <= expirydate)]
+                    fixture["date"] <= expirydate)
+        return [fixture for fixture in fixtures
+                if filterfn(fixture)]
 
+    """
+    - use teams supplied by user
+    - results=[]
+    """
+    
     @parse_json_body
     @emit_json
-    def post(self, req, results=[]): # NB no initial results state
+    def post(self, req, results=[]): 
         selectedteam=self.filter_selected_team(req["teams"])
-        teams=req["teams"] # requires validation
         allfixtures=fetch_fixtures(selectedteam["league"])
         expiry=init_expiry_date(allfixtures, req["expiry"])
-        fixtures=self.filter_fixtures(allfixtures, teams, expiry)
-        env={"teams": teams,
+        fixtures=self.filter_fixtures(allfixtures, req["teams"], expiry)
+        env={"teams": req["teams"],
              "results": results, 
              "fixtures": fixtures}
         probability=calc_probability(env, req["payoff"], selectedteam["name"])
