ecf16349e23fc28bf156ae711267c4d787f6ee41
Justin <justin.worrall@gmail.com>
2017-01-07 09:38:47 +0000

    added init expiries

--- js/app/single_teams.js
+++ js/app/single_teams.js
@@ -1,6 +1,35 @@
 var EESingleTeams={
+    initLeagues: function() {
+	$.ajax({
+	    dataType: "json",
+	    url: "/api/leagues",
+	    success: function(struct) {
+		var select=$("select[name='league']");
+		$(select).find("option").not(":first").remove();
+		$.each(struct, function(i, league) {
+		    var option=$("<option>").attr("value", league["name"]).text(league["name"]);
+		    $(select).append(option);
+		});
+	    }
+	});
+    },
+    initExpiries: function() {
+	$.ajax({
+	    dataType: "json",
+	    url: "/api/expiries",
+	    success: function(struct) {
+		var select=$("select[name='expiry']");
+		$(select).find("option").not(":first").remove();
+		$.each(struct, function(i, expiry) {
+		    var option=$("<option>").attr("value", expiry["value"]).text(expiry["label"]);
+		    $(select).append(option);
+		});
+	    }
+	});
+    },
     bind: function() {
-	console.log("Hello World!");
+	this.initLeagues();
+	this.initExpiries();
     }
 };
 
