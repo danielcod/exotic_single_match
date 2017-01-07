15beefc5ae4c44fd008835948f541d0370251c51
Justin <justin.worrall@gmail.com>
2017-01-07 07:33:30 +0000

    added mini leagues payoff

--- controllers/api/quant/positions/mini_leagues.py
+++ controllers/api/quant/positions/mini_leagues.py
@@ -1,5 +1,14 @@
 from controllers.api.quant.positions import *
 
+# curl "http://localhost:8080/api/quant/positions/mini_leagues/payoff"
+
+class PayoffHandler(webapp2.RequestHandler):
+
+    @emit_json
+    def get(self):
+        return [{"name": name}
+                for name in [Winner, Bottom]]
+    
 # curl -X POST "http://localhost:8080/api/quant/positions/mini_leagues/price" -d "{\"teams\": [{\"league\": \"ENG.1\", \"name\": \"Arsenal\", \"selected\": true}, {\"league\": \"SPA.1\", \"name\": \"Celta Vigo\"}, {\"league\": \"GER.1\", \"name\": \"Borussia Dortmund\"}], \"payoff\": \"Winner\", \"expiry\": \"2017-03-01\"}"
 
 class PriceHandler(webapp2.RequestHandler):
@@ -18,6 +27,7 @@ class PriceHandler(webapp2.RequestHandler):
         probability=calc_probability(env, req["payoff"], selectedteam["name"])
         return {"decimal_price": format_price(probability)}
 
-Routing=[('/api/quant/positions/mini_leagues/price', PriceHandler)]
+Routing=[('/api/quant/positions/mini_leagues/payoff', PayoffHandler),
+         ('/api/quant/positions/mini_leagues/price', PriceHandler)]
 
 app=webapp2.WSGIApplication(Routing)
