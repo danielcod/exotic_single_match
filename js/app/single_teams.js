20b82ea992df5d992c4f163a2478a47a6ae2caee
Justin <justin.worrall@gmail.com>
2017-01-07 09:54:40 +0000

    added teams, payoffs ajax loading

--- js/app/single_teams.js
+++ js/app/single_teams.js
@@ -27,7 +27,42 @@ var EESingleTeams={
 	    }
 	});
     },
+    initTeams: function(leaguename) {
+	$.ajax({
+	    dataType: "json",
+	    url: "/api/teams?league="+leaguename,
+	    success: function(struct) {
+		var select=$("select[name='team']");
+		$(select).find("option").not(":first").remove();
+		$.each(struct, function(i, team) {
+		    var option=$("<option>").attr("value", team["name"]).text(team["name"]);
+		    $(select).append(option);
+		});
+	    }
+	});
+    },
+    initPayoffs: function(leaguename) {
+	$.ajax({
+	    dataType: "json",
+	    url: "/api/quant/positions/single_teams/payoff?league="+leaguename,
+	    success: function(struct) {
+		var select=$("select[name='payoff']");
+		$(select).find("option").not(":first").remove();
+		$.each(struct, function(i, payoff) {
+		    var option=$("<option>").attr("value", payoff["name"]).text(payoff["name"]);
+		    $(select).append(option);
+		});
+	    }
+	});
+    },
     bind: function() {
+	$("select[name='league']").change(function() {
+	    var leaguename=$(this).find("option:selected").val();
+	    $("select[name='team'] option").not(":first").remove();
+	    EESingleTeams.initTeams(leaguename);
+	    $("select[name='payoff'] option").not(":first").remove();
+	    EESingleTeams.initPayoffs(leaguename);
+	});
 	this.initLeagues();
 	this.initExpiries();
     }
