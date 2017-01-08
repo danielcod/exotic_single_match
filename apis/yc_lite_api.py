6aacd8f496f9e5ee38bb5873a46633bbc92e0584
Justin <justin.worrall@gmail.com>
2017-01-08 05:03:48 +0000

    removed yc_lite get upcoming matches

--- apis/yc_lite_api.py
+++ apis/yc_lite_api.py
@@ -10,44 +10,33 @@ DefaultValuePricesLimit=10
 
 MemcacheAge=60*10
 
-def fetch_memcache(memkey, url, debug):
+def fetch_memcache(memkey, url):
     resp=memcache.get(memkey)
     if resp not in ['', None]:
-        if debug:
-            print "Serving %s from memcache" % memkey
         return json_loads(resp)
-    if debug:
-        print "Fetching %s" % url
     resp=rpc_get(url)
-    if debug:
-        print "Saving %s to memcache" % memkey
     memcache.add(memkey, json_dumps(resp), MemcacheAge)
     return resp    
 
-def get_leagues(debug=False):
+def get_leagues():
     memkey="leagues"
     url=Endpoint+"/api/leagues"
-    return fetch_memcache(memkey, url, debug)
+    return fetch_memcache(memkey, url)
 
-def get_teams(leaguename, debug=False):
+def get_teams(leaguename):
     memkey="%s/teams" % leaguename
     url=Endpoint+"/api/teams?league=%s" % urllib.quote(leaguename)
-    return fetch_memcache(memkey, url, debug)
+    return fetch_memcache(memkey, url)
 
-def get_upcoming_matches(leaguename, debug=False):
-    memkey="%s/upcoming_matches" % leaguename
-    url=Endpoint+"/api/upcoming_matches?league=%s" % urllib.quote(leaguename)
-    return fetch_memcache(memkey, url, debug)
-
-def get_remaining_fixtures(leaguename, debug=False):
+def get_remaining_fixtures(leaguename):
     memkey="%s/remaining_fixtures" % leaguename
     url=Endpoint+"/api/remaining_fixtures?league=%s" % urllib.quote(leaguename)
-    return fetch_memcache(memkey, url, debug)
+    return fetch_memcache(memkey, url)
 
-def get_results(leaguename, debug=False):
+def get_results(leaguename):
     memkey="%s/results" % leaguename
     url=Endpoint+"/api/results?league=%s" % urllib.quote(leaguename)
-    return fetch_memcache(memkey, url, debug)
+    return fetch_memcache(memkey, url)
 
 if __name__=="__main__":
     try:
@@ -62,13 +51,9 @@ if __name__=="__main__":
         leaguename, teamname = sys.argv[1:3]
         if not re.search("^\\D{3}\\.\\d$", leaguename):
             raise RuntimeError("League name is invalid")
-        print "%i leagues" % len(get_leagues(debug=True))
+        print "%i leagues" % len(get_leagues())
         print "%i teams" % len(get_teams(leaguename))
-        print "%i upcoming matches" % len(get_upcoming_matches(leaguename))
-        # print "%i remaining fixtures" % len(get_remaining_fixtures(leaguename))
-        remfixtures=get_remaining_fixtures(leaguename)
-        print "%i remaining fixtures" % len(remfixtures)
-        print remfixtures[0]
+        print "%i remaining fixtures" % len(get_remaining_fixtures(leaguename))
         print "%i results" % len(get_results(leaguename))
     except RuntimeError, error:
         print "Error: %s" % str(error)
