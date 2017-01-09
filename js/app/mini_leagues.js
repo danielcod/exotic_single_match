3f71677736bae9df7194eb397511978ee9a62dbb
Justin <justin.worrall@gmail.com>
2017-01-09 10:50:42 +0000

    cleaned up template

--- js/app/mini_leagues.js
+++ js/app/mini_leagues.js
@@ -59,49 +59,9 @@ var EEMiniLeagues={
 	    }
 	});
     },
-    isFormComplete: function() {
-	var complete=true;
-	$.each(["league", "team", "payoff", "expiry"], function(i, attr) {
-	    var value=$("select[name='"+attr+"'] option:selected").val();
-	    if (value=='') {
-		complete=false;
-	    };
-	});
-	return complete;
-    },
-    serialiseForm: function() {
-	var params={};
-	$.each(["league", "team", "payoff", "expiry"], function(i, attr) {
-	    params[attr]=$("select[name='"+attr+"'] option:selected").val();
-	});
-	return params;
-    },
-    updatePrice: function(params) {
-	$("span[name='price']").text("[updating ..]");
-	var struct={"product": "single_teams", // TEMP
-		    "query": params};
-	$.ajax({
-	    type: "POST",
-	    url: "/api/pricing",
-	    data: JSON.stringify(struct),
-	    contentType: "application/json",
-	    dataType: "json",
-	    success: function(struct) {
-		if (struct["decimal_price"]==null) {
-		    $("span[name='price']").text("[no price; sorry]");
-		} else {
-		    $("span[name='price']").text(struct["decimal_price"])
-		};
-	    }
-	});
-    },
     bind: function() {
 	// bind leagues
 	$("select[name='league']").change(function() {
-	    $("span[name='price']").text("[...]");
-	    $("select[name='team'] option").not(":first").remove();
-	    $("select[name='payoff'] option").not(":first").remove();
-	    $(this).find("option:first").prop("disabled", true);
 	    var leaguename=$(this).find("option:selected").val();
 	    if (leaguename!='') {
 		var row=$("table[name='your_team'] tbody tr:first");
@@ -111,27 +71,15 @@ var EEMiniLeagues={
 	});
 	// bind teams
 	$("select[name='team']").change(function() {
-	    $(this).find("option:first").prop("disabled", true);
-	    if (EEMiniLeagues.isFormComplete()) {
-		var params=EEMiniLeagues.serialiseForm();
-		EEMiniLeagues.updatePrice(params);
-	    };
+	    console.log("teams");
 	});
 	// bind payoffs
 	$("select[name='payoff']").change(function() {
-	    $(this).find("option:first").prop("disabled", true);
-	    if (EEMiniLeagues.isFormComplete()) {
-		var params=EEMiniLeagues.serialiseForm();
-		EEMiniLeagues.updatePrice(params);
-	    };
+	    console.log("payoffs")
 	});
 	// bind expiries
 	$("select[name='expiry']").change(function() {
-	    $(this).find("option:first").prop("disabled", true);
-	    if (EEMiniLeagues.isFormComplete()) {
-		var params=EEMiniLeagues.serialiseForm();
-		EEMiniLeagues.updatePrice(params);
-	    };
+	    console.log("expiries")
 	});
 	// init leagues
 	var row=$("table[name='your_team'] tbody tr:first");
