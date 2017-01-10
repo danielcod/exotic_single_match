275ea7c622fd491c417dd8f9bb7d8759a1b7cdfb
Justin <justin.worrall@gmail.com>
2017-01-10 08:31:42 +0000

    added back templates/site

--- controllers/site/single_teams.py
+++ controllers/site/single_teams.py
@@ -32,7 +32,7 @@ class IndexHandler(webapp2.RequestHandler):
                          for dep in Deps])
         tv={"title": Title,
             "deps": depsstr}
-        render_template(self, "templates/single_teams.html", tv)
+        render_template(self, "templates/site/single_teams.html", tv)
 
 Routing=[('/site/single_teams/payoff', PayoffHandler),
          ('/site/single_teams', IndexHandler)]
