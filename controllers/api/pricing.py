5a2828b3a7d856f189660f7650fca05351a8be36
Justin <justin.worrall@gmail.com>
2017-01-10 08:11:36 +0000

    cleanup

--- controllers/api/pricing.py
+++ controllers/api/pricing.py
@@ -2,6 +2,7 @@ from controllers.api import *
 
 from products.positions.single_teams import SingleTeamsProduct
 from products.positions.mini_leagues import MiniLeaguesProduct
+
 Products={
     "single_teams": SingleTeamsProduct,
     "mini_leagues": MiniLeaguesProduct
