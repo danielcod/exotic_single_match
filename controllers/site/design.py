ed8aa45e12298921c44a04977bc7ed82b94aee94
Justin <justin.worrall@gmail.com>
2017-01-11 11:49:56 +0000

    separated default product

--- controllers/site/design.py
+++ controllers/site/design.py
@@ -3,27 +3,30 @@ from controllers.site import *
 Products=yaml.load("""
 - label: Single Teams Outright
   name: single_teams
-  description: How Now Brown Cow
 - label: Mini Leagues
   name: mini_leagues
-  description: A Bird In the Hand 
 """)
 
 DesignDeps=Deps+["js/app/design.js"]
+
+DefaultProduct=yaml.load("""
+type: single_teams
+query:
+  league: ENG.1
+  team: Arsenal
+  payoff: Winner
+""")
 
 class InitHandler(webapp2.RequestHandler):
 
     @emit_json
     def get(self):
         return {"products": Products,
-                "product": {"type": "mini_leagues",
-                            "query": {"league": "ENG.1",
-                                      "team": "Arsenal",
-                                      "payoff": "Winner"}}}
+                "product": DefaultProduct}
 
 class IndexHandler(webapp2.RequestHandler):
     
-    def get(self):
+    def get(self):        
         depsstr=",".join(["\"../%s\"" % dep
                           for dep in DesignDeps])
         tv={"title": Title,
