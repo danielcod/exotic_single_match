bcd18d26e1df8275ca38e9a3450f33847497f5e3
Justin <justin.worrall@gmail.com>
2017-01-11 08:46:58 +0000

    added separate components for single teams, mini leagues

--- controllers/site/products.py
+++ controllers/site/products.py
@@ -1,5 +1,20 @@
 from controllers.site import *
 
+Products=yaml.load("""
+- label: Single Teams Outright
+  name: single_teams
+  description: How Now Brown Cow
+- label: Mini Leagues
+  name: mini_leagues
+  description: A Bird In the Hand 
+""")
+
+class ListHandler(webapp2.RequestHandler):
+
+    @emit_json
+    def get(self):
+        return Products
+
 class IndexHandler(webapp2.RequestHandler):
 
     def get(self):
@@ -9,7 +24,8 @@ class IndexHandler(webapp2.RequestHandler):
             "deps": depsstr}
         render_template(self, "templates/site/products.html", tv)
 
-Routing=[('/site/products', IndexHandler)]
+Routing=[('/site/products/list', ListHandler),
+         ('/site/products', IndexHandler)]
 
 app=webapp2.WSGIApplication(Routing)
 
--- js/app/exotics_engine.js
+++ js/app/exotics_engine.js
@@ -1,16 +1,51 @@
-var ProductsForm=React.createClass({
+var SingleTeamsForm=React.createClass({
     render: function() {	
-	return React.DOM.div({
+	return React.DOM.h3({
+	    children: React.DOM.span({
+		className: "label label-primary",
+		children: "Single Teams Form"
+	    })
+	});
+    }
+});
+
+var MiniLeaguesForm=React.createClass({
+    render: function() {	
+	return React.DOM.h3({
+	    children: React.DOM.span({
+		className: "label label-warning",
+		children: "Mini Leagues Form"
+	    })
+	});
+    }
+});
+
+var ProductsForm=React.createClass({
+    loadProductsSuccess: function(struct) {
+	console.log(JSON.stringify(struct));
+    },
+    loadProductsError: function(xhr, ajaxOptions, thrownError) {
+	console.log(xhr.responseText);
+    },
+    loadProducts: function(url) {
+	$.ajax({
+	    url: url,
+	    type: "GET",
+	    dataType: "json",
+	    success: this.loadProductsSuccess,
+	    error: this.loadProductsError
+	});
+    },
+    componentDidMount: function() {
+	this.loadProducts("/site/products/list");
+    },
+    render: function() {	
+	return React.DOM.div({	    
 	    className: "text-center",
 	    style: {
 		"margin-bottom": "30px"
 	    },
-	    children: React.DOM.h3({
-		children: React.DOM.span({
-		    className: "label label-primary",
-		    children: "Hello World!"
-		})
-	    })
+	    children: React.createElement(MiniLeaguesForm, {})
 	});
     }
 });
