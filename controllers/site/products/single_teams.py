ec22ca0334eaa388b7495a4b11654a2f8cffee0e
Justin <justin.worrall@gmail.com>
2017-01-11 21:11:20 +0000

    added single teams product

---
+++ controllers/site/products/single_teams.py
@@ -0,0 +1,19 @@
+from controllers.site.products import *
+
+from products.positions.single_teams import SingleTeamsProduct
+
+class PayoffHandler(webapp2.RequestHandler):
+
+    @validate_query({'league': '\\D{3}\\.\\d'})
+    @emit_json
+    def get(self):
+        leaguename=self.request.get("league")
+        product=SingleTeamsProduct()
+        payoffs=[{"value": name}
+                 for name in product.payoff_names(leaguename)]
+        return payoffs
+
+Routing=[('/site/products/single_teams/payoff', PayoffHandler)]
+
+app=webapp2.WSGIApplication(Routing)
+
