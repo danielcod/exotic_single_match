62c56a713496bfdaeb5d1145be87ba307726cf40
Justin <justin.worrall@gmail.com>
2017-01-16 14:22:30 +0000

    added app price

--- js/app/app.js
+++ js/app/app.js
@@ -1,5 +1,38 @@
+var AppPrice=React.createClass({
+    render: function() {
+	return React.DOM.h3({
+	    className: "text-center",
+	    style: {
+		color: "#888",
+		"margin-bottom": "20px"			
+	    },
+	    children: React.DOM.i({
+		children: [
+		    "Your price: ",
+		    React.DOM.span({
+			id: "price",
+			children: "[..]"
+		    })
+		]
+	    })			
+	});	
+    }
+});
+
+var App=React.createClass({
+    render: function() {
+	return React.DOM.div({
+	    children: [
+		React.createElement(AppPrice, {}),
+		React.createElement(ProductForm, {})
+	    ]
+	})
+    }
+});
+
+
 var Main=function() {
-    var app=React.createElement(ProductForm, {});
+    var app=React.createElement(App, {});
     var parent=$("div[id='app']")[0];
     ReactDOM.render(app, parent);
 };
