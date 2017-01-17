cddbc655286508346b8796650987d3a71c84d778
Justin <justin.worrall@gmail.com>
2017-01-17 09:56:27 +0000

    cleaned up single teams sample generator

--- tasks/random/single_teams.py
+++ tasks/random/single_teams.py
@@ -1,33 +1,7 @@
 from tasks.random import *
 
-from products.positions.single_teams import SingleTeamsProduct
-
-MinProb, MaxProb = 0.25, 0.75
-
 ProductName="single_teams"
 
-def payoff_group_name(payoffname):
-    if payoffname in ["Winner", "Promotion", "Relegation", "Bottom"]:
-        return "Main"
-    elif (payoffname.startswith("Top") or
-          payoffname.startswith("Outside Top")):
-        return "Top"
-    elif (payoffname.startswith("Bottom") or
-          payoffname.startswith("Outside Bottom")):
-        return "Bottom"
-    elif payoffname.endswith("Place"):
-        return "Place"
-    else:
-        raise RuntimeError("No group key for '%s'" % payoffname)
-
-def group_payoffs(payoff):
-    groups={}
-    for payoff in payoff:
-        groupname=payoff_group_name(payoff["name"])
-        groups.setdefault(groupname, [])
-        groups[groupname].append(payoff)
-    return groups
-        
 # curl "http://localhost:8080/tasks/random/single_teams?n=5"
 
 class IndexHandler(webapp2.RequestHandler):
@@ -50,36 +24,8 @@ class InitHandler(webapp2.RequestHandler):
         teams=yc_lite.get_teams(leaguename)
         i=int(len(teams)*random.random())
         teamname=teams[i]["name"]
-        product=SingleTeamsProduct()
-        allpayoffs=product.init_payoffs(leaguename, teamname)
-        filteredpayoffs=[payoff for payoff in allpayoffs
-                         if (payoff["value"] > MinProb and
-                             payoff["value"] < MaxProb)]        
-        payoffgroups=group_payoffs(filteredpayoffs)
-        payoffgroupnames=sorted(payoffgroups.keys())
-        if "Main" in payoffgroupnames:
-            payoffgroupname="Main"
-        else:
-            i=int(len(payoffgroupnames)*random.random())
-            payoffgroupname=payoffgroupnames[i]
-        payoffs=payoffgroups[payoffgroupname]
-        i=int(len(payoffs)*random.random())
-        payoffname=payoffs[i]["name"]
-        i=int(len(Expiries)*random.random())
-        expiryvalue=Expiries[i]["value"]        
-        struct={"league": leaguename,
-                "team": teamname,
-                "payoff": payoffname,
-                "expiry": expiryvalue}
-        priceresp=product.calc_price(struct)
-        price=priceresp[0]["value"]
-        Product(product=ProductName,
-                query=json_dumps(struct),
-                probability=price).put()
-        logging.info("%s: %s -> %.3f" % (ProductName,
-                                         struct,
-                                         price))
-
+        logging.info(leaguename+"/"+teamname)
+        
 Routing=[('/tasks/random/single_teams/init', InitHandler),
          ('/tasks/random/single_teams', IndexHandler)]
 
