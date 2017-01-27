22f4499227b9490add0cbb4d69a2666681e1a414
Justin <justin.worrall@gmail.com>
2017-01-27 12:25:29 +0000

    completed step 3

--- js/app/steps/step_three.js
+++ js/app/steps/step_three.js
@@ -28,46 +28,46 @@ var PlaceBetPanel=React.createClass({
 		}),
 		((this.state.params!=undefined) &&
 		 (this.state.confirmMessage==undefined)) ? [
-		    React.DOM.h3({			
-			className: "text-center",
-			style: {
+		     React.DOM.h3({			
+			 className: "text-center",
+			 style: {
+			     color: "#888"
+			 },
+			 children: React.DOM.b({
+			     children: this.state.params.description.selection
+			 })
+		     }),		    
+		     React.DOM.h3({
+			 className: "text-center",			
+			 style: {
+			     color: "#888"
+			 },
+			 children: this.state.params.description.market
+		     }),
+		     React.DOM.h3({
+			 className: "text-center",
+			 style: {
 			    color: "#888"
-			},
-			children: React.DOM.b({
-			    children: this.state.params.description.selection
-			})
-		    }),		    
-		    React.DOM.h3({
-			className: "text-center",			
-			style: {
-			    color: "#888"
-			},
-			children: this.state.params.description.market
-		    }),
-		    React.DOM.h3({
-			className: "text-center",
-			style: {
-			    color: "#888"
-			},
-			children: React.DOM.i({
-			    children: React.DOM.b({
-				children: "Your price: "+this.state.params.price
-			    })
-			})
-		    }),
-		    React.DOM.div({
-			className: "form-group",
-			children: [
-			    React.DOM.label({
-				children: "Your size"
-			    }),
-			    React.createElement(MyNumberInput, {
-				value: this.state.size,
-				changeHandler: this.sizeChangeHandler
-			    })
-			]
-		    })	    
-		] : undefined,
+			 },
+			 children: React.DOM.i({
+			     children: React.DOM.b({
+				 children: "Your price: "+this.state.params.price
+			     })
+			 })
+		     }),
+		     React.DOM.div({
+			 className: "form-group",
+			 children: [
+			     React.DOM.label({
+				 children: "Your size"
+			     }),
+			     React.createElement(MyNumberInput, {
+				 value: this.state.size,
+				 changeHandler: this.sizeChangeHandler
+			     })
+			 ]
+		     })	    
+		 ] : undefined,
 		(this.state.confirmMessage!=undefined) ? React.DOM.h3({
 		    className: "text-center",
 		    style: {
@@ -80,7 +80,7 @@ var PlaceBetPanel=React.createClass({
 			})
 		    })    
 		}) : undefined,
-		React.DOM.div({
+		(this.state.confirmMessage==undefined) ? React.DOM.div({
 		    className: "text-center",
 		    children: React.DOM.div({
 			className: "btn-group",
@@ -105,16 +105,33 @@ var PlaceBetPanel=React.createClass({
 				},
 				children: "Place Bet",
 				onClick: function() {
-				    if (this.state.confirmMessage==undefined) {
-					var state=this.state;
-					state.confirmMessage="Your bet has been placed!";
-					this.setState(state);
-				    } else {
-					this.props.stepChangeHandler(0, undefined);
-				    }
+				    var state=this.state;
+				    state.confirmMessage="Your bet has been placed!";
+				    this.setState(state);
 				}.bind(this)
 			    })
 			]
+		    })
+		}) : React.DOM.div({
+		    className: "text-center",
+		    children: React.DOM.div({
+			className: "btn-group",
+			style: {
+			    "margin-top": "20px"
+			},
+			children: React.DOM.button({
+			    className: "btn btn-primary",
+			    style: {
+				width: "100px"
+			    },
+			    children: "Bet Again",
+			    onClick: function() {
+				var state=this.state;
+				state.confirmMessage=undefined; // NB
+				this.setState(state);
+				this.props.stepChangeHandler(0, undefined);
+			    }.bind(this)
+			})
 		    })
 		})
 	    ]
