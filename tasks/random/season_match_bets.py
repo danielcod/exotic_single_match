db6cd74cb72a60a88e59080e5ee886f51acb271f
Justin <justin.worrall@gmail.com>
2017-02-03 10:47:22 +0000

    now calculating price

--- tasks/random/season_match_bets.py
+++ tasks/random/season_match_bets.py
@@ -1,10 +1,12 @@
 from tasks.random import *
 
-# from models.products.positions import calc_positional_probability
+from models.products.positions import calc_positional_probability
 
 import quant.simulator as simulator
 
 from models.products.positions.season_match_bets import SeasonMatchBetProduct
+
+Winner="Winner"
 
 def sumproduct(X, Y):
     return sum([(x*y) for x, y in zip(X, Y)])
@@ -32,7 +34,7 @@ class InitHandler(webapp2.RequestHandler):
         raise RuntimeError("Couldn't find versus team")        
 
     """
-    - this should be moved into product model
+    - this stuff should be moved into product model
     """
     
     def calc_pp_surface(self, leaguename, teams, expiry, paths=1000, seed=13):
@@ -50,27 +52,31 @@ class InitHandler(webapp2.RequestHandler):
                                   results,
                                   fixtures,
                                   paths, seed)
-    
-    """
-        teams=yc_lite.get_teams(leaguename)
-        i=int(len(teams)*random.random())
-        teamname=teams[i]["name"]
-        i=int(len(teams)*random.random())
-        versusname=self.init_versus(teamname, teams)
-        probability=0.1+0.8*random.random()
-        price=format_price(probability)
-        SeasonMatchBetProduct(league=leaguename,
-                              team=teamname,
-                              versus=versusname,
-                              expiry=expiry["value"],
-                              price=price).put()
-        logging.info("%s/%s/%s/%s -> %s" % (leaguename,
-                                            teamname,
-                                            versusname,
-                                            expiry["value"],
-                                            price))
-    """
-    
+
+    def calc_prices(self, leaguename, teamname, versusname, expiry, teams):
+        results=yc_lite.get_results(leaguename)        
+        fixtures=[{"name": fixture["name"],
+                   "date": fixture["date"],
+                   "probabilities": fixture["yc_probabilities"]}
+                   for fixture in [fixture.to_json()
+                                   for fixture in Fixture.find_all(leaguename)]]
+        today=datetime.date.today()
+        fixtures=[fixture for fixture in fixtures
+                  if (fixture["date"] > today and
+                      fixture["date"] < expiry["value"])]
+        struct={"team": {"league": leaguename,
+                         "name": teamname},
+                "teams": [team for team in teams
+                          if team["name"] in [teamname, versusname]],
+                "results": [result for result in results
+                            if (teamname in result["name"] or
+                                versusname in result["name"])],
+                "fixtures": [fixture for fixture in fixtures
+                             if (teamname in fixture["name"] or
+                                 versusname in fixture["name"])],
+                "payoffs": [{"name": "Winner"}]}
+        return calc_positional_probability(struct)[0]
+
     @task
     def post(self, std=3):        
         random.seed(random_seed())
@@ -83,12 +89,19 @@ class InitHandler(webapp2.RequestHandler):
         pp=sorted([(key, values)
                    for key, values in pp.items()],
                   key=lambda x: sumproduct(x[1], range(len(x[1]))))
+        teamnames=[row[0] for row in pp]
         i=std+int((len(teams)-(2*std))*random.random())
-        teamname=teams[i]["name"]
+        teamname=teamnames[i]
         f=1 if random.random() > 0.5 else -1
-        j=i+f*(1+int(random.random()*std))
-        versusname=teams[j]["name"]
+        j=i+f*(1+int((std-1)*random.random()))
+        versusname=teamnames[j]
         logging.info("%s/%s vs %s" % (leaguename, teamname, versusname))
+        allitems=self.calc_prices(leaguename,
+                                  teamname,
+                                  versusname,
+                                  expiry,
+                                  teams)
+        logging.info(allitems)
         
 Routing=[('/tasks/random/season_match_bets/init', InitHandler),
          ('/tasks/random/season_match_bets', IndexHandler)]
