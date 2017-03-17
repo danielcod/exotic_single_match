6cc82e488826a0201535ee3d0de12113123eef39
Justin <justin.worrall@gmail.com>
2017-03-17 13:51:01 +0000

    moved expiry selector to common components

--- js/app/components/bet_components.js
+++ js/app/components/bet_components.js
@@ -329,3 +329,51 @@ var LeagueTeamSelectorTable=React.createClass({
     }
 });
     
+var ExpirySelector=React.createClass({
+    initOptionsHandler: function(name) {
+	return function(struct) {
+	    var state=this.state;
+	    state.options[name]=struct;
+	    this.setState(state);	
+	}.bind(this);
+    },
+    getInitialState: function() {
+	return {
+	    options: {
+		expiry: []
+	    },
+	    expiry: this.props.expiry
+	};
+    },
+    fetchExpiries: function() {
+	var handler=this.initOptionsHandler("expiry");
+	this.props.exoticsApi.fetchExpiries(handler);
+    },
+    formatExpiryOptions: function(expiries) {
+	return expiries; // expiries come with label, value fields
+    },
+    changeHandler: function(name, value) {
+	if (this.state[name]!=value) {
+	    var state=this.state;
+	    state[name]=value;
+	    this.setState(state);
+	    this.props.changeHandler(name, value);
+	}
+    },
+    initialise: function() {
+	this.fetchExpiries();
+    },
+    componentDidMount: function() {
+	this.initialise();
+    },
+    render: function() {
+	return React.createElement(MySelect, {
+	    label: "At",
+	    name: "expiry",
+	    options: this.formatExpiryOptions(this.state.options.expiry),
+	    value: this.state.expiry,
+	    changeHandler: this.changeHandler,
+	    blankStyle: this.props.blankStyle
+	});
+    }
+});
