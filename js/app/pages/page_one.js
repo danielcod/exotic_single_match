3248bb758736a7ac66e131dc881bf4a776f1597a
Justin <justin.worrall@gmail.com>
2017-03-17 13:06:58 +0000

    added paginator

--- js/app/pages/page_one.js
+++ js/app/pages/page_one.js
@@ -161,77 +161,6 @@ var BrowseBetsTable=React.createClass({
     }
 });
 
-var BrowseBetsPaginator=React.createClass({
-    initItems: function(table, paginator, clickHandler) {
-	var nItems=Math.floor(table.nItems/table.nRows);
-	if (0 != table.nItems % table.nRows) {
-	    nItems+=1;
-	}
-	var currentItem=Math.floor(table.offset/table.nRows);
-	var firstItem=paginator.length*Math.floor(currentItem/paginator.length);
-	var lastItem=Math.min(nItems, firstItem+paginator.length);
-	/*
-	console.log(JSON.stringify({
-	    table: table,
-	    paginator: paginator,
-	    nItems: nItems,
-	    currentItem: currentItem,
-	    firstItem: firstItem,
-	    lastItem: lastItem
-	}));
-	*/
-	var items=[];
-	// '<<'
-	if (firstItem!=0) {
-	    items.push(React.DOM.li({
-		onClick: clickHandler.bind(null, (firstItem-1)*table.nRows),
-		children: React.DOM.a({
-		    children: React.DOM.span({
-			dangerouslySetInnerHTML: {
-			    "__html": "&laquo;"
-			}
-		    })
-		})
-	    }));
-	}
-	// numbered steps
-	for (var i=firstItem; i < lastItem; i++) {
-	    var item=React.DOM.li({
-		className: (currentItem==i) ? "active" : "",
-		onClick: clickHandler.bind(null, i*table.nRows),
-		children: React.DOM.a({
-		    children: (i+1).toString()
-		})
-	    })
-	    items.push(item);
-	}
-	// '>>'
-	if (lastItem!=nItems) {
-	    items.push(React.DOM.li({
-		onClick: clickHandler.bind(null, lastItem*table.nRows),
-		children: React.DOM.a({
-		    children: React.DOM.span({
-			dangerouslySetInnerHTML: {
-			    "__html": "&raquo;"
-			}
-		    })
-		})
-	    }))
-	}
-	return items;
-    },
-    render: function() {
-	return React.DOM.nav({
-	    children: React.DOM.ul({
-		className: "pagination pagination-sm",
-		children: this.initItems(this.props.table,
-					 this.props.paginator,
-					 this.props.clickHandler)
-	    })
-	});
-    }
-});
-
 var BrowseBetsPanel=React.createClass({
     getInitialState: function() {
 	return {
@@ -397,7 +326,7 @@ var BrowseBetsPanel=React.createClass({
 		}),
 		(this.state.nItems!=undefined) ? React.DOM.div({
 		    className: "text-center",
-		    children: React.createElement(BrowseBetsPaginator, {
+		    children: React.createElement(Paginator, {
 			table: {
 			    nItems:this.state.nItems,
 			    nRows: this.state.nRows,
