383e9e7f30a8cf270059a6593fed8f3c62515fbd
Justin <justin.worrall@gmail.com>
2017-03-26 12:23:45 +0100

    removed expiry helpers

--- controllers/app/expiries.py
+++ controllers/app/expiries.py
@@ -1,6 +1,23 @@
 from controllers.app import *
 
-from helpers.expiry_helpers import init_expiries
+def end_of_season(today):
+    if today.month < 7:
+        return datetime.date(today.year, 6, 30)
+    else:
+        return datetime.date(today.year+1, 6, 30)
+
+EndOfSeason=end_of_season(datetime.date.today())
+
+def init_expiries(offset=14):
+    date=datetime.date.today()+datetime.timedelta(days=offset)
+    items=[]
+    while True:
+        if date > EndOfSeason:
+            break
+        item={"value": date}
+        items.append(item)
+        date+=datetime.timedelta(days=1)
+    return items
 
 # curl "http://localhost:8080/app/expiries"
     
