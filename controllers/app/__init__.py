31eddd95a60ec1f1a601bb4b3885073880a30626
Justin <justin.worrall@gmail.com>
2017-03-26 12:08:08 +0100

    added dev/prod mode to root app controller

--- controllers/app/__init__.py
+++ controllers/app/__init__.py
@@ -2,19 +2,30 @@ from controllers import *
 
 import apis.yc_lite_api as yc_lite
 
-Deps=yaml.load(file("config/app_deps.yaml").read())
-
 Products=yaml.load(file("config/products.yaml").read())
 
 Leagues=yaml.load(file("config/leagues.yaml").read())
 
+Dev, Prod = "dev", "prod"
+
+DefaultMode=Prod
+
 class IndexHandler(webapp2.RequestHandler):
 
     def get(self):
-        depsstr=",".join(["\"../%s\"" % dep
-                          for dep in Deps])
-        tv={"deps": depsstr}
-        render_template(self, "templates/app.html", tv)
+        try:
+            mode=self.request.get("mode")
+            if mode in ["", None]:
+                mode=DefaultMode
+            if mode not in [Dev, Prod]:
+                raise RuntimeError("Mode not recognised")
+            deps=yaml.load(file("config/app_deps_%s.yaml" % mode).read())
+            depsstr=",".join(["\"../%s\"" % dep
+                              for dep in deps])
+            tv={"deps": depsstr}
+            render_template(self, "templates/app.html", tv)
+        except RuntimeError, error:
+            render_error(self, str(error))
     
 Routing=[('/app', IndexHandler)]
 
