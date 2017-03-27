c7bae64f49fc674ddf3eee39681e99a811c486cd
Justin <justin.worrall@gmail.com>
2017-03-27 11:59:30 +0100

    moved model imports into root directory

--- tasks/blobs/bets/__init__.py
+++ tasks/blobs/bets/__init__.py
@@ -1,4 +1,8 @@
 from tasks.blobs import *
 
+from models.bets.positions.mini_leagues import MiniLeagueBet
+from models.bets.positions.season_match_bets import SeasonMatchBet
+from models.bets.positions.single_team_outrights import SingleTeamOutrightBet
+
 Products=yaml.load(file("config/products.yaml").read())
 
