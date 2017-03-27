102de443c6ca5fd0ca08c861f78451b57732d8ff
Justin <justin.worrall@gmail.com>
2017-03-27 12:14:29 +0100

    removed price helpers

--- controllers/app/bets.py
+++ controllers/app/bets.py
@@ -4,7 +4,21 @@ from models.bets.positions.single_team_outrights import SingleTeamOutrightBet
 from models.bets.positions.season_match_bets import SeasonMatchBet
 from models.bets.positions.mini_leagues import MiniLeagueBet
 
-from helpers.price_helpers import format_price
+MaxProb, MinProb, MinPrice, MaxPrice = 0.99, 0.01, 1.001, 100
+
+def format_price(probability):
+    if probability < MinProb:
+        price=MaxPrice
+    elif probability > MaxProb:
+        price=MinPrice
+    else:
+        price=1/float(probability)
+    if price < 2:
+        return "%.3f" % price
+    elif price < 5:
+        return "%.2f" % price
+    else:
+        return "%.1f" % price
 
 """
 - this should be replaced by series of tasks generating interesting blobs
