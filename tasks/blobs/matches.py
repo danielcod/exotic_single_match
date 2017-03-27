387b6201be1f573a978f405ef0b6f4e1d06e6772
Justin <justin.worrall@gmail.com>
2017-03-27 10:33:24 +0100

    added match cutoff

--- tasks/blobs/matches.py
+++ tasks/blobs/matches.py
@@ -1,9 +1,10 @@
 from tasks.blobs import *
 
-# curl "http://localhost:8080/tasks/blobs/matches?leagues=ENG.1"
+# curl "http://localhost:8080/tasks/blobs/matches?window=7&leagues=ENG.1"
 
 class IndexHandler(webapp2.RequestHandler):
 
+    @validate_query({"window": "^\\d+$"})
     @task
     def get(self):         
         leaguenames=[leaguename 
@@ -11,8 +12,10 @@ class IndexHandler(webapp2.RequestHandler):
                      if leaguename in Leagues.keys()]
         if leaguenames==[]:
             leaguenames=Leagues.keys()
+        window=self.request.get("window")
         [taskqueue.add(url="/tasks/blobs/matches/map",
-                       params={"league": leaguename},
+                       params={"league": leaguename,
+                               "window": window},
                        queue_name=QueueName)
          for leaguename in leaguenames]
         logging.info("%s map tasks added" % len(leaguenames))
@@ -23,14 +26,18 @@ class IndexHandler(webapp2.RequestHandler):
 
 class MapHandler(webapp2.RequestHandler):
 
-    @validate_query({"league": "\\D{3}\\.\\d"})
+    @validate_query({"league": "\\D{3}\\.\\d",
+                     "window": "^\\d+$"})
     @task
     def post(self):
         leaguename=self.request.get("league")
+        window=int(self.request.get("window"))
+        cutoff=datetime.date.today()+datetime.timedelta(days=window)
         items=[{"league": leaguename,
                 "match": match["name"],
                 "kickoff": match["kickoff"]}
-               for match in yc_lite.get_upcoming_matches(leaguename)]
+               for match in yc_lite.get_upcoming_matches(leaguename)
+               if match["kickoff"].date() <= cutoff]
         keyname="matches/%s" % leaguename
         memcache.add(keyname, json_dumps(items), MemcacheAge)
         logging.info("Filtered %i %s matches" % (len(items), keyname))
