9f71f19650cacb963c3612e018f4dfa678c62318
Justin <justin.worrall@gmail.com>
2017-03-28 09:42:55 +0100

    added match team selector

--- js/app/components/selectors.js
+++ js/app/components/selectors.js
@@ -172,6 +172,100 @@ var TeamSelector=React.createClass({
     }
 });
 
+var MatchTeamSelector=React.createClass({
+    initOptionsHandler: function(name) {
+	return function(struct) {
+	    var state=this.state;
+	    state.options[name]=struct;
+	    this.setState(state);	
+	}.bind(this);
+    },
+    getInitialState: function() {
+	return {
+	    options: {
+		team: [],
+	    },
+	    item: JSON.parse(JSON.stringify(this.props.item)) // *** NB ***
+	};
+    },
+    fetchTeams: function() {
+	var handler=this.initOptionsHandler("team");
+	this.props.exoticsApi.fetchBlob("match_teams", handler);
+    },
+    sortTeams: function(item0, item1) {
+	var value0=item0.team+"/"+item0.date;
+	var value1=item1.team+"/"+item1.date;
+	if (value0 < value1) {
+	    return -1;
+	} else if (value0 > value1) {
+	    return 1;
+	} else {
+	    return 0;
+	}
+    },
+    formatTeamLabel: function(team) {
+	if ((team.league==undefined) ||
+	    (team.team==undefined)) {
+	    return undefined;
+	} else {
+	    return team.team+" (vs "+team.versus+" :: "+team.league+") ["+team.date+"]";
+	}
+    },
+    formatTeamValue: function(team) {
+	if ((team.league==undefined) ||
+	    (team.team==undefined)) {
+	    return undefined;
+	} else {
+	    return team.league+"/"+team.team;
+	}
+    },    
+    formatTeamOptions: function(teams) {
+	return teams.sort(this.sortTeams).map(function(team) {
+	    return {
+		label: this.formatTeamLabel(team),
+		value: this.formatTeamValue(team)
+	    }
+	}.bind(this));
+    },
+    changeHandler: function(name, value) {
+	var tokens=value.split("/");
+	var leaguename=tokens[0];
+	var teamname=tokens[1];
+	var state=this.state;
+	state.item.league=leaguename;
+	state.item.team=teamname;
+	this.setState(state);
+	this.props.changeHandler(state.item);
+    },
+    componentWillReceiveProps: function(nextProps) {
+	if (JSON.stringify(this.state.item)!=
+	    JSON.stringify(nextProps.item)) {
+	    var state=this.state;
+	    state.item=nextProps.item;
+	    this.setState(state);
+	}
+    },
+    initialise: function() {
+	this.fetchTeams();
+    },
+    componentDidMount: function() {
+	this.initialise();
+    },
+    render: function() {
+	return React.createElement(MySelect, {
+	    changeHandler: this.changeHandler,
+	    options: this.formatTeamOptions(this.state.options.team),
+	    value: this.formatTeamValue(this.state.item),
+	    // pass thru attributes
+	    blankStyle: this.props.blankStyle,
+	    className: this.props.className,
+	    defaultOption: this.props.defaultOption,
+	    label: this.props.label,
+	    name: this.props.name || "team"
+	});
+    }
+});
+
 var MatchSelector=React.createClass({
     initOptionsHandler: function(name) {
 	return function(struct) {
@@ -193,9 +287,11 @@ var MatchSelector=React.createClass({
 	this.props.exoticsApi.fetchBlob("matches", handler);
     },
     sortMatches: function(item0, item1) {
-	if (item0.match < item1.match) {
+	var value0=item0.match+"/"+item0.date;
+	var value1=item1.match+"/"+item1.date;
+	if (value0 < value1) {
 	    return -1;
-	} else if (item0.match > item1.match) {
+	} else if (value0 > value1) {
 	    return 1;
 	} else {
 	    return 0;
