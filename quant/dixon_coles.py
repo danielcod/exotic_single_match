9ca13275ff187b53c7008cd73f96cfd355b86338
Justin <justin.worrall@gmail.com>
2017-04-08 09:28:45 +0100

    now using random instance

--- quant/dixon_coles.py
+++ quant/dixon_coles.py
@@ -2,6 +2,8 @@
 http://localhost:8888/notebooks/notebooks/sportshacker/dixon_coles_adjustment.ipynb
 """
 
+from random import Random
+
 import math
 
 HomeWin, Draw, AwayWin = MOSelections = ["home_win", "draw", "away_win"]
@@ -34,12 +36,12 @@ class CSGrid(list):
 
     @classmethod
     def solve(self, probs, n=N, generations=1000, seed=13):
-        import random
-        random.seed(seed)
+        rand=Random()
+        rand.seed(seed)
         l, besterr = [1, 1], 1e10
         for i in range(generations):
             factor, j = ((generations-i)/float(generations))**2, i%2
-            q=factor*random.gauss(0, 1)
+            q=factor*rand.gauss(0, 1)
             l[j]+=q
             grid=CSGrid(l[0], l[1], n)
             err=rms_error([grid.match_odds(selection)
@@ -71,6 +73,8 @@ class CSGrid(list):
         else:
             raise RuntimeError("No filter fn for '%s'" % selection)
         return self.sum(filterfn)
+
+    
     
 if __name__=="__main__":
     try:
@@ -81,6 +85,11 @@ if __name__=="__main__":
         grid=CSGrid(lx, ly)
         for selection in MOSelections:        
             print "%s: %.5f" % (selection, grid.match_odds(selection))
+        """
+        lx, ly =  1.23533, 0.66517
+        grid=CSGrid(lx, ly)
+        print grid
+        """
     except RuntimeError, error:
         print "Error: %s" % str(error)
         
