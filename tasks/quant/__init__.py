cc244dbe68d95897e8cbddf7fec6375482c3830c
Justin <justin.worrall@gmail.com>
2017-04-13 16:02:54 +0100

    cleaned up quant init

--- tasks/quant/__init__.py
+++ tasks/quant/__init__.py
@@ -5,10 +5,3 @@ import apis.yc_lite_api as yc_lite
 Leagues=dict([(league["name"], league)
               for league in yaml.load(file("config/leagues.yaml").read())])
 
-SDKwargs=yaml.load("""
-realm: prod
-username: admin
-password: Hufton123
-appengine: true
-""")
-
