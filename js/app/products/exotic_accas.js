7c658484a02ae5e8800c72641a8fd25e7ff5569c
Justin <justin.worrall@gmail.com>
2017-06-06 10:49:20 +0100

    removed condition_selector.js

--- js/app/products/exotic_accas.js
+++ js/app/products/exotic_accas.js
@@ -23,6 +23,60 @@ var ExoticAccaGridLayout=React.createClass({
     }
 });
 
+var ExoticAccaConditionSelector=React.createClass({
+    getInitialState: function() {
+	return {
+	    options: {
+		condition: [
+		    {
+			label: "More Than",
+			value: ">"
+		    },
+		    {
+			label: "At Least",
+			value: ">="
+		    },
+		    {
+			label: "Exactly",
+			value: "="
+		    },
+		    {
+			label: "Less Than",
+			value: "<"
+		    },
+		    {
+			label: "At Most",
+			value: "<="
+		    }
+		]
+	    },
+	    condition: this.props.condition
+	};
+    },    
+    changeHandler: function(name, value) {
+	if (this.state[name]!=value) {
+	    var state=this.state;
+	    state[name]=value;
+	    this.setState(state);
+	    this.props.changeHandler(name, value);
+	}
+    },
+    render: function() {
+	return React.createElement(MySelect, {
+	    changeHandler: this.changeHandler,
+	    options: this.state.options.condition,
+	    value: this.state.condition,
+	    // pass thru attributes
+	    blankStyle: this.props.blankStyle,
+	    className: this.props.className,
+	    defaultOption: this.props.defaultOption,	    
+	    label: this.props.label || "Condition",
+	    name: this.props.name || "condition"
+	});
+    }
+});
+
+
 var ExoticAccaForm=React.createClass({
     initOptionsHandler: function(name) {
 	return function(struct) {
@@ -153,7 +207,7 @@ var ExoticAccaForm=React.createClass({
 		    }
 		}),
 		[
-		    React.createElement(ConditionSelector, {
+		    React.createElement(ExoticAccaConditionSelector, {
 			label: "Teams Condition",
 			name: "teams_condition",
 			condition: this.state.bet.params.teams_condition,
