76213400e937daf40ade3007cb6f2abef8aa7fc5
Justin <justin.worrall@gmail.com>
2017-06-08 12:08:56 +0100

    moved grid layout

--- js/app/products/exotic_accas.js
+++ js/app/products/exotic_accas.js
@@ -1,28 +1,3 @@
-var ExoticAccaGridLayout=React.createClass({
-    initItem: function(item, colwidth) {
-	return React.DOM.div({
-	    className: "col-xs-"+colwidth,
-	    children: item
-	});
-    },
-    initRow: function(row) {
-	var colwidth=12/row.length;
-	return React.DOM.div({
-	    className: "row",
-	    children: Array.isArray(row) ? row.map(function(item) {		
-		return this.initItem(item, colwidth)
-	    }.bind(this)) : this.initItem(row, 12)
-	});
-    },
-    render: function() {
-	return React.DOM.div({
-	    children: this.props.rows.map(function(row) {
-		return this.initRow(row);
-	    }.bind(this))
-	})
-    }
-});
-
 var ExoticAccaConditionSelector=React.createClass({
     getInitialState: function() {
 	return {
@@ -122,6 +97,31 @@ var ExoticAccaNTeamsSlider=React.createClass({
 		})
 	    ]
 	})	
+    }
+});
+
+var ExoticAccaGridLayout=React.createClass({
+    initItem: function(item, colwidth) {
+	return React.DOM.div({
+	    className: "col-xs-"+colwidth,
+	    children: item
+	});
+    },
+    initRow: function(row) {
+	var colwidth=12/row.length;
+	return React.DOM.div({
+	    className: "row",
+	    children: Array.isArray(row) ? row.map(function(item) {		
+		return this.initItem(item, colwidth)
+	    }.bind(this)) : this.initItem(row, 12)
+	});
+    },
+    render: function() {
+	return React.DOM.div({
+	    children: this.props.rows.map(function(row) {
+		return this.initRow(row);
+	    }.bind(this))
+	})
     }
 });
 
