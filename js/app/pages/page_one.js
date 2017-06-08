c1f2ff22dda1b85516c16c6c5c583b4ae8521339
Justin <justin.worrall@gmail.com>
2017-06-08 15:06:28 +0100

    moved selectors into underlying pages

--- js/app/pages/page_one.js
+++ js/app/pages/page_one.js
@@ -1,3 +1,178 @@
+var BrowseBetsProductSelector=React.createClass({
+    initOptionsHandler: function(name) {
+	return function(struct) {
+	    var state=this.state;
+	    state.options[name]=struct;
+	    this.setState(state);	
+	}.bind(this);
+    },
+    getInitialState: function() {
+	return {
+	    options: {
+		product: []
+	    },
+	    product: this.props.product
+	};
+    },
+    fetchProducts: function() {
+	var handler=this.initOptionsHandler("product");
+	this.props.exoticsApi.fetchProducts(handler);
+    },
+    sortProducts: function(item0, item1) {
+	if (item0.type < item1.type) {
+	    return -1;
+	} else if (item0.type > item1.type) {
+	    return 1;
+	} else {
+	    return 0;
+	}
+    },
+    formatProductLabel: function(product) {
+	if (product==undefined) {
+	    return undefined;
+	} else {
+	    return product.label;
+	}
+    },
+    formatProductValue: function(product) {
+	if (product==undefined) {
+	    return undefined;
+	} else {
+	    return product.type;
+	}
+    },
+    formatProductOptions: function(products) {
+	return products.sort(this.sortProducts).map(function(product) {
+	    return {
+		label: this.formatProductLabel(product),
+		value: this.formatProductValue(product)
+	    }
+	}.bind(this));
+    },
+    changeHandler: function(name, value) {
+	if (this.state[name]!=value) {
+	    var state=this.state;
+	    state[name]=value;
+	    this.setState(state);
+	    this.props.changeHandler(name, value);
+	}
+    },
+    initialise: function() {
+	this.fetchProducts();
+    },
+    componentDidMount: function() {
+	this.initialise();
+    },
+    render: function() {
+	return React.createElement(MySelect, {
+	    changeHandler: this.changeHandler,
+	    options: this.formatProductOptions(this.state.options.product),
+	    value: this.formatProductValue(this.state.product),
+	    // pass thru attributes
+	    blankStyle: this.props.blankStyle,
+	    className: this.props.className,
+	    defaultOption: this.props.defaultOption,
+	    label: this.props.label,
+	    name: this.props.name || "product"
+	});
+    }
+});
+
+
+var BrowseBetsTeamSelector=React.createClass({
+    initOptionsHandler: function(name) {
+	return function(struct) {
+	    var state=this.state;
+	    state.options[name]=struct;
+	    this.setState(state);	
+	}.bind(this);
+    },
+    getInitialState: function() {
+	return {
+	    options: {
+		team: [],
+	    },
+	    item: this.props.item
+	};
+    },
+    fetchTeams: function() {
+	var handler=this.initOptionsHandler("team");
+	this.props.exoticsApi.fetchBlob("app/teams", handler);
+    },
+    sortTeams: function(item0, item1) {
+	if (item0.team < item1.team) {
+	    return -1;
+	} else if (item0.team > item1.team) {
+	    return 1;
+	} else {
+	    return 0;
+	}
+    },
+    formatTeamLabel: function(team) {
+	if ((team.league==undefined) ||
+	    (team.team==undefined)) {
+	    return undefined;
+	} else {
+	    return team.team+" ("+team.league+")";
+	}
+    },
+    formatTeamValue: function(team) {
+	if ((team.league==undefined) ||
+	    (team.team==undefined)) {
+	    return undefined;
+	} else {
+	    return team.league+"/"+team.team;
+	}
+    },    
+    formatTeamOptions: function(teams) {
+	return teams.sort(this.sortTeams).map(function(team) {
+	    return {
+		label: this.formatTeamLabel(team),
+		value: this.formatTeamValue(team)
+	    }
+	}.bind(this));
+    },
+    changeHandler: function(name, value) {
+	var tokens=value.split("/");
+	var leaguename=tokens[0];
+	var teamname=tokens[1];
+	var state=this.state;
+	state.item.league=leaguename;
+	state.item.team=teamname;
+	this.setState(state);
+	this.props.changeHandler(state.item);
+    },
+    componentWillReceiveProps: function(nextProps) {
+	if (JSON.stringify(this.state.item)!=
+	    JSON.stringify(nextProps.item)) {
+	    var state=this.state;
+	    state.item=nextProps.item;
+	    this.setState(state);
+	}
+    },
+    initialise: function() {
+	this.fetchTeams();
+    },
+    componentDidMount: function() {
+	this.initialise();
+    },
+    render: function() {
+	return React.createElement(MySelect, {
+	    changeHandler: this.changeHandler,
+	    options: this.formatTeamOptions(this.state.options.team),
+	    value: this.formatTeamValue(this.state.item),
+	    // pass thru attributes
+	    blankStyle: this.props.blankStyle,
+	    className: this.props.className,
+	    defaultOption: this.props.defaultOption,
+	    label: this.props.label,
+	    name: this.props.name || "team"
+	});
+    }
+});
+
+
+
 var BrowseBetsInlineList=React.createClass({
     render: function() {
 	return React.DOM.ul({
@@ -263,7 +438,7 @@ var BrowseBetsPanel=React.createClass({
 		React.createElement(BrowseBetsInlineList, {
 		    items: [
 			{
-			    item: React.createElement(TeamSelector, {
+			    item: React.createElement(BrowseBetsTeamSelector, {
 				exoticsApi: this.props.exoticsApi,
 				className: "form-control input-sm btn-secondary",
 				item: {},
@@ -277,7 +452,7 @@ var BrowseBetsPanel=React.createClass({
 			    width: 100
 			},
 			{
-			    item: React.createElement(ProductSelector, {
+			    item: React.createElement(BrowseBetsProductSelector, {
 				exoticsApi: this.props.exoticsApi,
 				className: "form-control input-sm btn-secondary",
 				item: {},
