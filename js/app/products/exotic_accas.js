a337288896b0a11493c9b775e9bb8b7bdd5c780f
Justin <justin.worrall@gmail.com>
2017-06-12 11:27:51 +0100

    moved date selector stuff

--- js/app/products/exotic_accas.js
+++ js/app/products/exotic_accas.js
@@ -1,3 +1,35 @@
+var ExoticAccaDateUtils={
+    formatMonth: function(date) {
+	var months=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	return months[date.getMonth()]; // NB JS months indexed at zero
+    },
+    formatDaySuffix: function(date) {
+	var day=date.getDate();
+	if ((day==1) || (day==21) || (day==31)) {
+	    return "st";
+	} else if ((day==2) || (day==22)) {
+	    return "nd";
+	} else if ((day==3) || (day==23)) {
+	    return "rd";
+	} else {
+	    return "th";
+	}
+    },
+    formatDate: function(date) {
+	var month=this.formatMonth(date);
+	var day=date.getDate();
+	var suffix=this.formatDaySuffix(date);
+	return month+" "+day+suffix;
+    },
+    formatMinutes: function(date) {
+	var minutes=date.getMinutes();
+	return (minutes < 10) ? '0'+minutes : minutes;
+    },
+    formatTime: function(date) {
+	return date.getHours()+":"+this.formatMinutes(date);
+    }
+};
+
 var ExoticAccaSelectorTabs=React.createClass({
     render: function() {
 	return React.DOM.ul({
@@ -142,38 +174,6 @@ var ExoticAccaTeamSelectionCell=React.createClass({
 	});
     }
 });
-
-var ExoticAccaDateUtils={
-    formatMonth: function(date) {
-	var months=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-	return months[date.getMonth()]; // NB JS months indexed at zero
-    },
-    formatDaySuffix: function(date) {
-	var day=date.getDate();
-	if ((day==1) || (day==21) || (day==31)) {
-	    return "st";
-	} else if ((day==2) || (day==22)) {
-	    return "nd";
-	} else if ((day==3) || (day==23)) {
-	    return "rd";
-	} else {
-	    return "th";
-	}
-    },
-    formatDate: function(date) {
-	var month=this.formatMonth(date);
-	var day=date.getDate();
-	var suffix=this.formatDaySuffix(date);
-	return month+" "+day+suffix;
-    },
-    formatMinutes: function(date) {
-	var minutes=date.getMinutes();
-	return (minutes < 10) ? '0'+minutes : minutes;
-    },
-    formatTime: function(date) {
-	return date.getHours()+":"+this.formatMinutes(date);
-    }
-};
 
 var ExoticAccaDateTimeCell=React.createClass({
     render: function() {
