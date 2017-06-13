525370536ea33768388dda9f929ccc40b66410ef
Justin <justin.worrall@gmail.com>
2017-06-13 12:34:49 +0100

    removed product selcetor

--- js/app/pages/page_two.js
+++ js/app/pages/page_two.js
@@ -1,84 +1,3 @@
-var EditBetProductSelector=React.createClass({
-    initOptionsHandler: function(name) {
-	return function(struct) {
-	    var state=this.state;
-	    state.options[name]=struct;
-	    this.setState(state);	
-	}.bind(this);
-    },
-    getInitialState: function() {
-	return {
-	    options: {
-		product: []
-	    },
-	    product: this.props.product
-	};
-    },
-    fetchProducts: function() {
-	var handler=this.initOptionsHandler("product");
-	this.props.exoticsApi.fetchProducts(handler);
-    },
-    sortProducts: function(item0, item1) {
-	if (item0.type < item1.type) {
-	    return -1;
-	} else if (item0.type > item1.type) {
-	    return 1;
-	} else {
-	    return 0;
-	}
-    },
-    formatProductLabel: function(product) {
-	if (product==undefined) {
-	    return undefined;
-	} else {
-	    return product.label;
-	}
-    },
-    formatProductValue: function(product) {
-	if (product==undefined) {
-	    return undefined;
-	} else {
-	    return product.type;
-	}
-    },
-    formatProductOptions: function(products) {
-	return products.sort(this.sortProducts).map(function(product) {
-	    return {
-		label: this.formatProductLabel(product),
-		value: this.formatProductValue(product)
-	    }
-	}.bind(this));
-    },
-    changeHandler: function(name, value) {
-	if (this.state[name]!=value) {
-	    var state=this.state;
-	    state[name]=value;
-	    this.setState(state);
-	    this.props.changeHandler(name, value);
-	}
-    },
-    initialise: function() {
-	this.fetchProducts();
-    },
-    componentDidMount: function() {
-	this.initialise();
-    },
-    render: function() {
-	return React.createElement(MySelect, {
-	    changeHandler: this.changeHandler,
-	    options: this.formatProductOptions(this.state.options.product),
-	    value: this.formatProductValue(this.state.product),
-	    // pass thru attributes
-	    blankStyle: this.props.blankStyle,
-	    className: this.props.className,
-	    defaultOption: this.props.defaultOption,
-	    label: this.props.label,
-	    name: this.props.name || "product"
-	});
-    }
-});
-
-
 var EditBetForm=React.createClass({
     initBet: function(bet) {	
 	return (bet!=undefined) ? bet : {
@@ -121,15 +40,6 @@ var EditBetForm=React.createClass({
 	state.price=price;
 	this.setState(state);
     },
-    loadProductForm: function(type) {
-	for(var i=0 ; i < this.state.products.length; i++) {
-	    var product=this.state.products[i];
-	    if (type==product.type) {
-		return eval(product.form);
-	    }
-	}
-	return undefined;
-    },
     updatePrice: function(bet) {
 	this.priceChangeHandler("[updating ..]");
 	this.props.exoticsApi.fetchPrice(bet, function(struct) {
@@ -149,17 +59,6 @@ var EditBetForm=React.createClass({
 	return React.DOM.div({
 	    children: [
 		React.DOM.div({
-		    style: {
-			"margin-top": "20px"
-		    },		   
-		    children: React.createElement(EditBetProductSelector, {
-			exoticsApi: this.props.exoticsApi,
-			className: "form-control btn-primary input-lg",
-			product: this.state.bet,
-			changeHandler: this.productChangeHandler
-		    })
-		}),
-		React.DOM.div({
 		    className: "form-group",
 		    children: React.DOM.h3({
 			className: "current-price text-center",
@@ -172,7 +71,7 @@ var EditBetForm=React.createClass({
 			]				    
 		    })
 		}),
-		(this.state.products.length!=0) ? React.createElement(this.loadProductForm(this.state.bet.type), {
+		(this.state.products.length!=0) ? React.createElement(ExoticAccaForm, {
 		    exoticsApi: this.props.exoticsApi,
 		    bet: this.state.bet,
 		    blankStyle: {
