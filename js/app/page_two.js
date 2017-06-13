86b67821ae54a5a5cc5d9d2bef7eaae096f0dfd7
Justin <justin.worrall@gmail.com>
2017-06-13 13:37:05 +0100

    cleaned up pricing code and next button code

--- js/app/page_two.js
+++ js/app/page_two.js
@@ -470,7 +470,6 @@ var ExoticAccaForm=React.createClass({
 	var state=this.state;
 	state.bet.params.teams=teams;
 	this.setState(state);
-	this.updatePrice(this.state.bet);
     },
     changeHandler: function(name, value) {
 	console.log(name+" -> "+value);
@@ -482,7 +481,6 @@ var ExoticAccaForm=React.createClass({
 		state.bet.params[name]=value;
 	    }
 	    this.setState(state);
-	    this.updatePrice(this.state.bet);
 	}
     },
     isComplete: function(bet) {
@@ -512,16 +510,7 @@ var ExoticAccaForm=React.createClass({
 	}
 	return true;
     },
-    updatePrice: function(bet) {
-	if (this.isComplete(bet)) {
-	    this.props.updatePriceHandler(bet)
-	} else {
-	    this.props.resetPriceHandler();
-	}
-    },
     componentDidMount: function() {
-	// update price
-	this.updatePrice(this.state.bet);
 	// load matches
 	this.props.exoticsApi.fetchBlob("app/matches", function(struct) {
 	    var state=this.state;
@@ -591,69 +580,18 @@ var EditBetForm=React.createClass({
     },
     getInitialState: function() {
 	return {
-	    bet: this.initBet(this.props.bet),
-	    price: "[..]"
+	    bet: this.initBet(this.props.bet)
 	}
-    },
-    componentDidMount: function() {
-    },
-    productChangeHandler: function(name, value) {
-	var state=this.state;
-	state.bet={
-	    type: value,
-	    params: {},
-	    probability: undefined,
-	    description: undefined
-	}
-	this.setState(state);
-    },
-    formatPrice: function(probability) {
-	return (1/Math.min(0.99, Math.max(0.01, probability))).toFixed(2);
-    },
-    priceChangeHandler: function(price) {
-	var state=this.state;
-	state.price=price;
-	this.setState(state);
-    },
-    updatePrice: function(bet) {
-	this.priceChangeHandler("[updating ..]");
-	this.props.exoticsApi.fetchPrice(bet, function(struct) {
-	    var state=this.state;
-	    state.bet.probability=struct.probability;
-	    state.bet.description=struct.description;
-	    this.setState(state);
-	    this.priceChangeHandler(this.formatPrice(struct.probability));
-	}.bind(this));
-	this.props.productChangeHandler(bet);
-    },
-    resetPrice: function() {
-	this.priceChangeHandler("[..]");
-	this.props.productChangeHandler(undefined);
     },
     render: function() {
 	return React.DOM.div({
 	    children: [
-		React.DOM.div({
-		    className: "form-group",
-		    children: React.DOM.h3({
-			className: "current-price text-center",
-			children: [
-			    "Current price: ",
-			    React.DOM.span({
-				id: "price",
-				children: this.state.price
-			    })					
-			]				    
-		    })
-		}),
 		React.createElement(ExoticAccaForm, {
 		    exoticsApi: this.props.exoticsApi,
 		    bet: this.state.bet,
 		    blankStyle: {
 			border: "3px solid #59a0df"
-		    },
-		    updatePriceHandler: this.updatePrice,
-		    resetPriceHandler: this.resetPrice
+		    }
 		})
 	    ]
 	});
@@ -661,51 +599,12 @@ var EditBetForm=React.createClass({
 });
 
 var EditBetPanel=React.createClass({
-    getInitialState: function() {
-	return {
-	    bet: this.props.bet
-	}
-    },
-    productChangeHandler: function(struct) {
-	var state=this.state;
-	state.bet=struct;
-	this.setState(state);
-    },
-    deepCopy: function(struct) {
-	if (struct==undefined) {
-	    return struct;
-	}
-	return JSON.parse(JSON.stringify(struct));
-    },
     render: function() {
 	return React.DOM.div({
 	    children: [
 		React.createElement(EditBetForm, {
 		    exoticsApi: this.props.exoticsApi,
-		    productChangeHandler: this.productChangeHandler,
-		    bet: this.deepCopy(this.props.bet) // don't want original bet to be updated; create a copy
-		}),
-		React.DOM.div({
-		    className: "text-center",
-		    style: {
-			margin: "5px 0 20px"
-		    },
-		    children: [
-			React.DOM.button({
-			    className: "btn btn-primary",
-			    style: {
-				width: "100px",
-				"margin-left": "3px"
-			    },
-			    children: "Next",
-			    onClick: function() {
-				if (this.state.bet!=undefined) {
-				    // this.props.stepChangeHandler(1, this.state.bet);
-				    console.log("next step!");
-				}
-			    }.bind(this)
-			})
-		    ]
+		    bet: this.props.bet
 		})
 	    ]
 	});
