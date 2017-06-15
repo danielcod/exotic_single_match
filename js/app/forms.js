c43de3d095a2df25b8e4887133af92828fdbbab9
Justin <justin.worrall@gmail.com>
2017-06-15 10:26:47 +0100

    simplified MySelect

--- js/app/forms.js
+++ js/app/forms.js
@@ -1,26 +1,14 @@
 var MySelect=React.createClass({
-    getInitialState: function() {
-	return {
-	    value: this.props.value
-	}
-    },
     renderSelect: function() {
 	return React.DOM.select({
 	    className: "form-control",
-	    value: this.state.value,
-	    style: (this.state.value==undefined) ? this.props.blankStyle : {},
 	    onChange: function(event) {
-		var value=(event.target.value!="") ? event.target.value : undefined;
-		var state=this.state;
-		state.value=value;
-		this.setState(state);
-		this.props.changeHandler(this.props.name, value);
+		this.props.changeHandler(this.props.name, event.target.value);
 	    }.bind(this),
 	    children: [
 		this.props.options.map(function(option) {
 		    return React.DOM.option({
 			value: option.value,
-			selected: option.value==this.state.value,
 			children: option.label || option.value
 		    })
 		}.bind(this))
