ec08423fb730d03606924d114e23af583ee88031
Justin <justin.worrall@gmail.com>
2017-06-18 11:20:29 +0100

    renamed items as matches

--- tasks/app/matches.py
+++ tasks/app/matches.py
@@ -52,7 +52,7 @@ class MapHandler(webapp2.RequestHandler):
         leaguename=self.request.get("league")
         window=int(self.request.get("window"))
         cutoff=datetime.date.today()+datetime.timedelta(days=window)
-        items=sorted([{"league": leaguename,
+        matches=sorted([{"league": leaguename,
                        "name": match["name"],
                        "kickoff": match["kickoff"],
                        "1x2_prices": normalise_1x2_prices(filter_best_1x2_prices(match["pre_event_1x2_prices"]))}
@@ -65,8 +65,8 @@ class MapHandler(webapp2.RequestHandler):
                           match["pre_event_1x2_prices"] not in [[], None])],
                      key=lambda x: "%s/%s" % (x["kickoff"], x["name"]))
         keyname="matches/%s" % leaguename
-        memcache.set(keyname, json_dumps(items), MemcacheAge)
-        logging.info("Filtered %i %s matches" % (len(items), keyname))
+        memcache.set(keyname, json_dumps(matches), MemcacheAge)
+        logging.info("Filtered %i %s matches" % (len(matches), keyname))
 
 class ReduceHandler(webapp2.RequestHandler):
 
@@ -74,16 +74,16 @@ class ReduceHandler(webapp2.RequestHandler):
     @task
     def post(self):
         leaguenames=self.request.get("leagues").split(",")
-        items=[]
+        matches=[]
         for leaguename in leaguenames:
             keyname="matches/%s" % leaguename
             resp=memcache.get(keyname)
             if resp in ['', None, []]:
                 continue
-            items+=json_loads(resp)
-        logging.info("Total %i matches" % len(items))
+            matches+=json_loads(resp)
+        logging.info("Total %i matches" % len(matches))
         Blob(key_name="app/matches",
-             text=json_dumps(items),
+             text=json_dumps(matches),
              timestamp=datetime.datetime.now()).put()
         logging.info("Saved to /matches")
                 
