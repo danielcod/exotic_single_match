a53fbdacb5a775ed0349e5efdcd3109d7faf2ed7
Justin <justin.worrall@gmail.com>
2017-06-18 10:51:21 +0100

    added bets endpoint

---
+++ controllers/app/bets.py
@@ -0,0 +1,16 @@
+from controllers.app import *
+
+# curl -X POST "http://localhost:8080/app/bets/price" -d "foo=bar"
+
+class PriceHandler(webapp2.RequestHandler):
+
+    @emit_json
+    def post(self):
+        import random
+        price=1/float(0.1+random.random()*0.8)
+        return {"price": "%.2f" % price}
+        
+Routing=[('/app/bets/price', PriceHandler)]
+
+app=webapp2.WSGIApplication(Routing)
+
