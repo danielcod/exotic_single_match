60cc53eaeba123836a1efe0eb64d347c471c93f4
Justin <justin.worrall@gmail.com>
2017-06-18 19:19:51 +0100

    updated exotic accaa code

--- dev/exotic_acca.py
+++ dev/exotic_acca.py
@@ -1,4 +1,12 @@
-from quant.dixon_coles import CSGrid
+# from quant.dixon_coles import CSGrid
+
+# START TEMP CODE
+
+from random import Random
+
+import math
+
+# END TEMP CODE
 
 Win, Lose, Draw = "win", "lose", "draw"
 
@@ -14,97 +22,151 @@ Conditions={
 
 Paths, Seed = 1000, 13
 
-def validate_quant(fn):
-    def wrapped_fn(fixtures, *args, **kwargs):
-        errors=[]
-        for fixture in fixtures:
-            if fixture.dc_poisson_means in ['', None, []]:
-                errors.append("%s/%s" % (fixture.league, fixture.name))
-        if errors!=[]:
-            raise RuntimeError("Following fixtures are missing poisson means: %s" % ", ".join(errors))
-        return fn(fixtures, *args, **kwargs)
-    return wrapped_fn
+"""
+- using a custom copy of CSGrid because existing version in quant/dixon_coles.py only accepts poisson means as constructor args
+"""
 
-@validate_quant
+class CSGrid(list):
+
+    """
+    def __init__(self, lx, ly, n=N):
+        list.__init__(self, [[poisson(lx, i)*poisson(ly, j)
+                              for j in range(n)]
+                             for i in range(n)])
+    """
+
+    def __init__(self, values):
+        list.__init__(self, values)
+
+    def sum(self, filterfn):
+        return sum([self[i][j]                    
+                    for i in range(len(self))
+                    for j in range(len(self))
+                    if filterfn(i, j)])
+
+    def match_odds(self, selection):
+        if selection==HomeWin:
+            filterfn=lambda i, j: i > j
+        elif selection==Draw:
+            filterfn=lambda i, j: i==j
+        elif selection==AwayWin:
+            filterfn=lambda i, j: i < j
+        else:
+            raise RuntimeError("No filter fn for '%s'" % selection)
+        return self.sum(filterfn)
+    
+    def simulate(self, paths, seed):
+        def flatten(grid):
+            items, prob = [], 0
+            for i in range(len(grid)):
+                for j in range(len(grid)):
+                    prob+=self[i][j]
+                    item=[(i, j), prob]
+                    items.append(item)
+            items[-1][1]=1.0 # NB
+            return items
+        def simulate(items, rand):
+            startprob, endprob = 0, None
+            q=rand.random()
+            for item in items:
+                endprob=item[1]
+                if (q > startprob and
+                    q <= endprob):
+                    return item[0]
+                startprob=endprob
+        rand=Random()
+        rand.seed(seed)        
+        items=flatten(self)    
+        return [simulate(items, rand)
+                for i in range(paths)]
+
 def simulate_match_teams(fixtures, paths, seed):
     items=[{} for i in range(paths)]
     for fixture in fixtures:
-        grid=CSGrid(*tuple(fixture.dc_poisson_means))
+        # grid=CSGrid(*tuple(fixture.dc_poisson_means))
+        grid=CSGrid(fixture["dc_grid"])
         for i, score in enumerate(grid.simulate(paths, seed)):
             teamnames=fixture.name.split(" vs ")
             items[i][teamnames[0]]=(score[0], score[1])
             items[i][teamnames[1]]=(score[1], score[0])
     return items
 
-def match_filterfn(params):
+def leg_filterfn(bet):
     errmsg="'%s' is invalid goals condition for match '%s' status"
     def filterfn(score):
-        if params["result"]==Win:
-            if params["goals_condition"]==GT:                
-                return score[0]-score[1] > params["n_goals"]
-            elif params["goals_condition"]==GTE:                
-                return score[0]-score[1] >= params["n_goals"]
-            elif params["goals_condition"]==LT:
-                return score[0]-score[1] < params["n_goals"]
-            elif params["goals_condition"]==LTE:
-                return score[0]-score[1] <= params["n_goals"]
-            elif params["goals_condition"]==EQ:                
-                return (score[0]-score[1])==params["n_goals"]
+        if bet["resultCondition"]==Win:
+            if bet["goalsCondition"]==GT:                
+                return score[0]-score[1] > bet["nGoals"]
+            elif bet["goalsCondition"]==GTE:                
+                return score[0]-score[1] >= bet["nGoals"]
+            elif bet["goalsCondition"]==LT:
+                return score[0]-score[1] < bet["nGoals"]
+            elif bet["goalsCondition"]==LTE:
+                return score[0]-score[1] <= bet["nGoals"]
+            elif bet["goalsCondition"]==EQ:                
+                return (score[0]-score[1])==bet["nGoals"]
             else:
-                raise RuntimeError(errmsg % (params["goals_condition"], Win))
-        elif params["result"]==Lose:
-            if params["goals_condition"]==GT:
-                return score[1]-score[0] > params["n_goals"]
-            elif params["goals_condition"]==GTE:
-                return score[1]-score[0] >= params["n_goals"]
-            elif params["goals_condition"]==LT:
-                return score[1]-score[0] < params["n_goals"]
-            elif params["goals_condition"]==LTE:
-                return score[1]-score[0] <= params["n_goals"]
-            elif params["goals_condition"]==EQ:
-                return (score[1]-score[0])==params["n_goals"]
+                raise RuntimeError(errmsg % (bet["goalsCondition"], Win))
+        elif bet["resultCondition"]==Lose:
+            if bet["goalsCondition"]==GT:
+                return score[1]-score[0] > bet["nGoals"]
+            elif bet["goalsCondition"]==GTE:
+                return score[1]-score[0] >= bet["nGoals"]
+            elif bet["goalsCondition"]==LT:
+                return score[1]-score[0] < bet["nGoals"]
+            elif bet["goalsCondition"]==LTE:
+                return score[1]-score[0] <= bet["nGoals"]
+            elif bet["goalsCondition"]==EQ:
+                return (score[1]-score[0])==bet["nGoals"]
             else:
-                raise RuntimeError(errmsg % (params["goals_condition"], Lose))
-        elif params["result"]==Draw:
-            if params["goals_condition"]==EQ:
-                return score[0]==score[1]==params["n_goals"]
+                raise RuntimeError(errmsg % (bet["goalsCondition"], Lose))
+        elif bet["resultCondition"]==Draw:
+            if bet["goalsCondition"]==EQ:
+                return score[0]==score[1]==bet["nGoals"]
             else:
-                raise RuntimeError(errmsg % (params["goals_condition"], Draw))
+                raise RuntimeError(errmsg % (bet["goalsCondition"], Draw))
         else:
-            raise RuntimeError("Params result not found/recognised")
+            raise RuntimeError("Bet result not found/recognised")
     return filterfn
 
-def params_filterfn(params):
-    matchfilterfn=match_filterfn(params)
-    def bool_to_int(value):
-        return 1 if value else 0
+def bool_to_int(value):
+    return 1 if value else 0
+
+def bet_filterfn(bet):
+    legfilterfn=leg_filterfn(bet)
     def filterfn(scores):
-        outcomes=[matchfilterfn(scores[team["team"]])
-                  for team in params["teams"]]
+        """
+        this needs to change to iterate over existing leg structure and information thereby provided
+        """        
+        outcomes=[legfilterfn(scores[team["team"]])
+                  for team in bet["teams"]]
         n=len([outcome for outcome in outcomes
                if outcome])
-        if params["teams_condition"]==GT:
-            return bool_to_int(n > params["n_teams"])
-        elif params["teams_condition"]==GTE:
-            return bool_to_int(n >= params["n_teams"])
-        elif params["teams_condition"]==LT:
-            return bool_to_int(n < params["n_teams"])
-        elif params["teams_condition"]==LTE:
-            return bool_to_int(n <= params["n_teams"])
-        elif params["teams_condition"]==EQ:
-            return bool_to_int(n==params["n_teams"])
+        if bet["legsCondition"]==GT:
+            return bool_to_int(n > bet["nLegs"])
+        elif bet["legsCondition"]==GTE:
+            return bool_to_int(n >= bet["nLegs"])
+        elif bet["legsCondition"]==LT:
+            return bool_to_int(n < bet["nLegs"])
+        elif bet["legsCondition"]==LTE:
+            return bool_to_int(n <= bet["nLegs"])
+        elif bet["legsCondition"]==EQ:
+            return bool_to_int(n==bet["nLegs"])
         else:
-            raise RuntimeError("%s is invalid teams condition" % params["teams_condition"])
+            raise RuntimeError("%s is invalid legs condition" % bet["legsCondition"])
     return filterfn
 
-def calc_probability(params, paths=Paths, seed=Seed):
+def calc_probability(bet, paths=Paths, seed=Seed):
+    """
+    this needs to change to filter matches from list of supplied matches
+    """
     matches=[Fixture.get_by_key_name("%s/%s" % (team["league"],
                                                 team["match"]))
-             for team in params["teams"]]
+             for team in bet["teams"]]
     samples=simulate_match_teams(matches, paths, seed)
-    filterfn=params_filterfn(params)
-    return sum([filterfn(scores)
-                for scores in samples])/float(paths)
+    filterfn=bet_filterfn(bet)
+    return sum([filterfn(sample)
+                for sample in samples])/float(paths)
 
 import datetime 
 
@@ -113,7 +175,12 @@ SampleBet={u'nLegs': 3, u'nGoals': 2, u'bust': 4509327226, u'legs': [{u'selectio
 if __name__=="__main__":
     from helpers.json_helpers import json_loads
     matches=json_loads(file("dev/matches.json").read())
-    print matches
+    import copy
+    bet=copy.deepcopy(SampleBet)
+    bet["resultCondition"]=Win
+    bet["legsCondition"]=GTE
+    bet["goalsCondition"]=GTE
+    print bet
     
     
 
