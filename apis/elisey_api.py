a9a1c76c98d6e846283d12634a9605b70e284079
Justin <justin.worrall@gmail.com>
2017-07-07 09:59:01 +0100

    added elisey api with auto retry

--- apis/elisey_api.py
+++ apis/elisey_api.py
@@ -1,28 +1,45 @@
 from apis import *
 
+import yaml
+
 Endpoint="http://iosport-exotics-api.appspot.com/_ah/api/main/v1"
 
 ApiKey="AIzaSyAUCQTKkAEJ7CufQyr4yvrmxLaJRSOmsZg"
 
+Bookmakers=yaml.load("""
+- bet365
+""")
+
 Timeout=30 
 
-def rpc_get(url):
+"""
+- rpc_get incorporates built- in 500 retry for simple handling of Elisey API timeouts
+"""
+
+def rpc_get(url, maxtries=5, wait=1):
     args={"method": "GET",
           "url": url,
           "headers": {"Accept": "application/json"},
           "deadline": Timeout}
-    http=urlfetch.fetch(**args)
-    if http.status_code==400:
-        raise RuntimeError(http.content)
-    if http.status_code!=200:
-        raise RuntimeError("Server returned HTTP %i" % http.status_code)
-    return json_loads(http.content)
+    import time
+    for i in range(maxtries):
+        http=urlfetch.fetch(**args)
+        if http.status_code==200:
+            return json_loads(http.content)            
+        elif http.status_code==400:
+            raise RuntimeError(http.content)
+        elif 5==http.status_code/100:
+            time.sleep(wait)
+        else:
+            raise RuntimeError("Server returned HTTP %i" % http.status_code)
+    raise RuntimeError("Max tries exceeded")
 
 def unpack_items(fn):
     def wrapped_fn(*args, **kwargs):
         resp=fn(*args, **kwargs)
         if "items" not in resp:
-            raise RuntimeError("No items found")
+            # raise RuntimeError("No items found")
+            resp["items"]=[]
         return resp["items"]
     return wrapped_fn
 
@@ -35,12 +52,12 @@ def get_teams(leaguename, season, key=ApiKey):
     return rpc_get(Endpoint+"/league_table?league=%s&season=%s&key=%s" % (leaguename, season, key))
 
 @unpack_items
-def get_results(leaguename, season, key=ApiKey):
-    return rpc_get(Endpoint+"/results?league=%s&season=%s&key=%s" % (leaguename, season, key))
+def get_results(leaguename, season, bookmakers=Bookmakers, key=ApiKey):
+    return rpc_get(Endpoint+"/results?league=%s&season=%s&bookmakers=%s&key=%s" % (leaguename, season, ",".join(bookmakers), key))
 
 @unpack_items
-def get_remaining_fixtures(leaguename, season, key=ApiKey):
-    return rpc_get(Endpoint+"/remaining_fixtures?league=%s&season=%s&key=%s" % (leaguename, season, key))
+def get_remaining_fixtures(leaguename, season, bookmakers=Bookmakers, key=ApiKey):
+    return rpc_get(Endpoint+"/remaining_fixtures?league=%s&season=%s&bookmakers=%s&key=%s" % (leaguename, season, ",".join(bookmakers), key))
 
 if __name__=="__main__":
     try:
