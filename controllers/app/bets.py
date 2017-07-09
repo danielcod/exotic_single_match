86ed82d986f54d671720b664531eb1da80e78745
Justin <justin.worrall@gmail.com>
2017-07-09 07:38:37 +0100

    added function on bet pricing side to add pricing conditions

--- controllers/app/bets.py
+++ controllers/app/bets.py
@@ -4,17 +4,23 @@ import quant.exotic_acca as exotic_acca
 
 Seed, Paths = 13, 5000
 
+ExoticAccaWinner="exotic_acca_winner"
+
 class PriceHandler(webapp2.RequestHandler):
 
+    def update_bet(self, bet):
+        if bet["name"]==ExoticAccaWinner:
+            bet["resultCondition"]=exotic_acca.Win
+            bet["legsCondition"]=exotic_acca.GTE
+            bet["goalsCondition"]=exotic_acca.GTE
+            
     @parse_json_body
     @emit_json
     def post(self, bet, limit=0.005):
-        logging.info(bet)
-        bet["resultCondition"]=exotic_acca.Win
-        bet["legsCondition"]=exotic_acca.GTE
-        bet["goalsCondition"]=exotic_acca.GTE
-        matches=Blob.fetch("app/matches")                
-        prob=exotic_acca.calc_probability(bet, matches,
+        self.update_bet(bet)
+        matches=Blob.fetch("app/matches")
+        prob=exotic_acca.calc_probability(bet,
+                                          matches,
                                           seed=Seed,
                                           paths=Paths)
         price=1/float(max(limit, prob))
