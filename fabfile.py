370fe5078074b295f76440f766bb94c7d9aa2159
Daniel <daniel.liu717@gmail.com>
2017-08-03 12:20:12 +0200

    Original file

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 import os, re, yaml
 
-#AppEngineHome="~/packages/google-cloud-sdk/platform/google_appengine"
-AppEngineHome = r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\platform\google_appengine"'
+AppEngineHome="~/packages/google-cloud-sdk/platform/google_appengine"
+#AppEngineHome = r'"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\platform\google_appengine"'
 PathToDatastore = "tmp/dev_appserver.datastore"
 Email = "justin.worrall@gmail.com"
 DevPort, DevAdminPort = 8080, 8000
