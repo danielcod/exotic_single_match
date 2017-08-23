9582e2b54fa20ece191fd172de52ced98f8e69ed
eganichev <evgenii.ganichev@gmail.com>
2017-08-23 16:58:49 +0300

    change style Team Cards Template

--- js/app/src/components/organisms/TeamCardsPanel/TeamCardsPanel.js
+++ js/app/src/components/organisms/TeamCardsPanel/TeamCardsPanel.js
@@ -12,7 +12,7 @@ import classNames from 'classnames';
 export default class TeamCardsPanel extends React.PureComponent {
         constructor(props){
         super(props);
-        bindAll(this, ['handleTabClicked', 'clickGrid', 'decrementValue', 
+        bindAll(this, ['handleTabClicked',  'decrementValue', 
                         'incrementValue', 'handleCancel', 'formatText', 
                         'changeStateByTab', 'setToParrenState', 'handleCancel']);
         let bet = this.getCurrentBet(this.props); 
@@ -91,44 +91,26 @@ export default class TeamCardsPanel extends React.PureComponent {
             selectedBetTab: pos,
             changes: true
         })
+
+      setTimeout(()=>this.formatText(), 0); 
     }
     handleTabClicked(tab) {
-        this.setState({selectedTab: tab.name, changes: true});
+        this.setState({selectedTab: tab.name});
         setTimeout(()=>this.formatText(), 0); 
     }
-    clickGrid(event){
-        const gridName = event.target.innerText;
-        let position;
-        if (!gridName) return;
-        switch(gridName){
-            case constant.HOME_TEAM:
-                position = 0;                
-                break;
-            case constant.AWAY_TEAM:
-                position = 1;
-                break;
-            case constant.BOTH_TEAM:
-                position = 2;
-                break;                
-            case constant.MATCH_TOTAL:
-                position = 3;
-                break;
-        }        
-        this.changeStateByTab(position)
-        setTimeout(()=>this.formatText(), 0); 
-    }
+    
     decrementValue() {        
         const {sliderOptions} = this.state;
         let toogleValue = this.state.toogleValue - sliderOptions.step;
         if (toogleValue < sliderOptions.min) toogleValue = this.state.toogleValue;
-        this.setState({toogleValue, changes: true});        
+        this.setState({toogleValue});        
         setTimeout(()=>this.formatText(), 0); 
     }
     incrementValue() {          
        const {sliderOptions} = this.state;
         let toogleValue = this.state.toogleValue + sliderOptions.step;
         if (toogleValue > sliderOptions.max) toogleValue = this.state.toogleValue;
-        this.setState({toogleValue, changes: true});
+        this.setState({toogleValue});
         setTimeout(()=>this.formatText(), 0); 
 
     }
@@ -147,31 +129,44 @@ export default class TeamCardsPanel extends React.PureComponent {
        this.setState({textValue});
        setTimeout(()=>this.setToParrenState(), 0) ;
    }
-    render(){
-        const toogleValue = this.state.toogleValue + ' Cards';
+    render(){        
+        const firstTeam = this.props.match.name.split(' vs ')[0];
+        const secondTeam = this.props.match.name.split(' vs ')[1];
+        const toogleValue = this.state.toogleValue;
         return(
             <div>
-                <table className={s['cornersTable']}>
-                    <tbody>
-                        <tr>
-                            {
-                                products.cornersComponents.map((value, key)=>{                                                                     
-                                    return(
-                                        <td  key={key} 
-                                            className={(this.state.selectedBetTab === key) ? 
-                                                classNames(s['item'], s['selected-bet-tab']) 
-                                                : classNames(s['item'])}
-                                            onClick={this.clickGrid}>
-                                            <span >
-                                                {value} 
-                                            </span> 
-                                    </td>)
-                                })
-                            } 
-                            
-                        </tr>
-                    </tbody>
-                </table>
+                <div className={s['cornersTable']}>
+                    <div className={classNames('row', s['row-change-btn'])}>
+                        <div className={(this.state.selectedBetTab === 0) ? 
+                                            classNames(s['item'], s['selected-bet-tab'], 'col-xs-6', 'col-md-6') 
+                                            : classNames(s['item'], s['not-selected-bet-tab'], 'col-xs-6', 'col-md-6')}
+                            onClick={()=>this.changeStateByTab(constant.SELCTED_FIRST)}>
+                            {firstTeam}
+                        </div>
+                        <div className={(this.state.selectedBetTab === 1) ? 
+                                            classNames(s['item'], s['selected-bet-tab'], 'col-xs-6', 'col-md-6') 
+                                            : classNames(s['item'], s['not-selected-bet-tab'], 'col-xs-6', 'col-md-6')}
+                            onClick={()=>this.changeStateByTab(constant.SELCTED_TWO)}>
+                            {secondTeam}
+                        </div>
+                    </div>
+                    <div className={classNames('row', s['row-change-btn'])}>
+                        <div className={(this.state.selectedBetTab === 2) ? 
+                                            classNames(s['item'], s['selected-bet-tab'], 'col-xs-6', 'col-md-6') 
+                                            : classNames(s['item'], s['not-selected-bet-tab'], 'col-xs-6', 'col-md-6')}
+                            onClick={()=>this.changeStateByTab(constant.SELCTED_THREE)}>
+                            {products.cornersComponents[2]}
+                        </div>
+                        <div className={(this.state.selectedBetTab === 3) ? 
+                                            classNames(s['item'], s['selected-bet-tab'], 'col-xs-6', 'col-md-6') 
+                                            : classNames(s['item'], s['not-selected-bet-tab'], 'col-xs-6', 'col-md-6')}
+                            onClick={()=>this.changeStateByTab(constant.SELCTED_FOUR)}>
+                            {products.cornersComponents[3]}
+                        </div>
+                        
+                        
+                    </div>
+                </div>
                 <div className={s['wrap-mybettab']}>
                     <MyBetTab
                         tabs={this.state.myBetTab}
@@ -188,19 +183,16 @@ export default class TeamCardsPanel extends React.PureComponent {
                             decrement: this.decrementValue
                         }}/>
                 </div>
-                <div
-                    className={s['show-text']}>
-                    {this.state.textValue}
-                </div>
-                <div className= "form-group">
+               
+                <div className={classNames(s["form-group"], s['output-block'])}>
                     {
                         (!this.state.changes) ? 
                         <h3 className= "current-price text-center">
                            No Selections
                         </h3>
                         :
-                        <h3 className= "current-price text-center">
-                            Selection Price: 
+                        <h3 className={classNames(s['output-block'], "current-price", "text-center")} >
+                             {this.state.textValue}: 
                             <span className={s['price']} id= "price">
                                 { this.state.price }
                             </span>
@@ -210,7 +202,7 @@ export default class TeamCardsPanel extends React.PureComponent {
                 <div className={classNames("bet-submit-btns", s['btn-group'])}>
                     <button
                         className="btn btn-primary bet-cancel-btn"
-                        onClick={() => this.handleCancel()}>Clear Selections
+                        onClick={() => this.handleCancel()}>Clear
                     </button>
                 </div>
             </div>
