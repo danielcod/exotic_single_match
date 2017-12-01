6053455b48c8631cb156dfab2b99f06502918481
Daniel <daniel.liu717@gmail.com>
2017-12-01 16:46:41 +0100

    Show/Hide badge in app tab

--- js/app/src/components/pages/App/AppStage/AppTab/AppTab.js
+++ js/app/src/components/pages/App/AppStage/AppTab/AppTab.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import {formatCountBets} from '../../../../utils';
-import classnames from 'classnames';
-import * as s from './index.css';
+import React from 'react'
+import {formatCountBets} from '../../../../utils'
+import classnames from 'classnames'
+import * as s from './index.css'
 export default class AppTab extends React.PureComponent {
 
     render() {
-        const { bets, match } = this.props;        
-        const betInBets = formatCountBets(bets, match);        
+        const { bets, match } = this.props
+        const betInBets = formatCountBets(bets, match)
         return (
             <div className="main-menu">
                 <ul className="nav nav-tabs">
                     {
                         this.props.tabs.map((tab, key) => {
-                            const myClass = classnames((tab.name == this.props.selected) ? "active" : "", 
+                            const myClass = classnames((tab.name === this.props.selected) ? "active" : "",
                                                         tab.name === "bet" ? s['badgeBet'] : null)
                             return (
                                 <li key={key}
@@ -20,7 +20,7 @@ export default class AppTab extends React.PureComponent {
                                     onClick={this.props.clickHandler.bind(null, tab)}>
                                     <a href='#'>
                                         {tab.label}
-                                        {(tab.name === "bet") ?
+                                        {(tab.name === "bet" && betInBets > 0) ?
                                             <span className={classnames("badge", s['badge_'])}>
                                                 {betInBets}
                                             </span>
